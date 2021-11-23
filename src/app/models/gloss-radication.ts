6c255dd5abcf539adc399c8640a84b79252896a8
Jorge Galeano <gidesarrolladorapp1@hlips.com.co>
2021-11-23 16:02:46 -0500

    registro de usuario en respuesta y radicacion de glosas

--- src/app/models/gloss-radication.ts
+++ src/app/models/gloss-radication.ts
@@ -2,6 +2,7 @@ import { MainClass } from './main-class';
 
 export class GlossRadication extends MainClass {
   id: number;
+  user_id: number;
   gloss_response_id: number;
   radication_date: string;
   observation: string;
--- src/app/models/gloss-response.ts
+++ src/app/models/gloss-response.ts
@@ -2,6 +2,7 @@ import { MainClass } from './main-class';
 
 export class GlossResponse extends MainClass {
   id: number;
+  user_id: number;
   gloss_id: number;
   objetion_code_response_id: number;
   objetion_response_id: number;
