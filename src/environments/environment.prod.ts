da654c69d563c9a34df3696f9298a2cf80da71fd
desarrollador <gidesarrollador@hlips.com.co>
2021-11-26 08:00:44 -0500

    datos de configuración

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,16 +3,19 @@
  * Copyright Akveo. All Rights Reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
-export const environment = {
+ export const environment = {
   production: false,
-  api: 'https://sga-api-test.eduredejrlb.co/api/',
   cas: 'https://sso-test.eduredejrlb.co/cas/',
-  storage: 'http://34.71.7.149:8001/storage/',
-  api_absolute: 'http://34.71.7.149:8001/api-ensename-ejrlb/public/',
+  api: 'http://192.168.0.204:8000/api/',
+  storage: 'http://192.168.0.204:8000/storage/',
+  api_absolute: 'http://192.168.0.204/api-ensename-ejrlb/public/',
   app_version: 'v1.0.7',
   url_lms: 'https://ejrlb.territorio.la/integraciones/Login/LoginGotoCourses/?assertion_data=',
-  url_lms_ratings: 'https://lms-test.eduredejrlb.co/blocks/sga_integrations/controller.php?method=extractGradesAndCompetences',
+
+  url_service: 'http://localhost',
+  login_sso: 'false',  // sso -> Login por cas,ensename -> login directo
+  url_lms_ratings: ' https://lms-test.eduredejrlb.co/blocks/sga_integrations/controller.php?method=extractGradesAndCompetences',
   url_lms_enrollment: 'https://lms-test.eduredejrlb.co/blocks/sga_integrations/controller.php?method=processSgaIntegration&debug=false',
-  url_service: 'https://sga-test.eduredejrlb.co',
-  login_sso: 'true',  //sso -> Login por cas, ensename -> login directo
+
 };
+
