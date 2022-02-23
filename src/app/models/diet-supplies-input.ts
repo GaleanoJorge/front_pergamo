baf0394d1905c131bd8f8b29573cb2d0c68a95f7
Jorge Galeano <gidesarrolladorapp1@hlips.com.co>
2022-02-23 16:53:45 -0500

    formulario de estrada de insumo

---
+++ src/app/models/diet-supplies-input.ts
@@ -0,0 +1,10 @@
+import { MainClass } from './main-class';
+
+export class DietSuppliesInput extends MainClass {
+  id: number;
+  amount: number;
+  price: number;
+  invoice_number: string;
+  diet_supplies_id: number;
+  company_id: number;
+}
