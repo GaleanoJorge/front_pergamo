1b2cf6599327faac4f57cd7185338f563ce0ca3b
Jorge Galeano <gidesarrolladorapp1@hlips.com.co>
2022-02-24 07:39:31 -0500

    formulario de asignación de menús a pacientes

---
+++ src/app/pages/diets/diets-list/form-diets/form-diets.component.ts
@@ -0,0 +1,63 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { NbDialogRef, NbToastrService } from '@nebular/theme';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { DietMenuDishService } from '../../../../business-controller/diet-menu-dish.service';
+
+
+@Component({
+  selector: 'ngx-form-diets',
+  templateUrl: './form-diets.component.html',
+  styleUrls: ['./form-diets.component.scss'],
+})
+export class FormDietsComponent implements OnInit {
+  @Input() title: string;
+  @Input() data: any = null;
+
+  public form: FormGroup;
+  public loading: boolean = false;
+  public isSubmitted: boolean = false;
+  public saved: any = null;
+
+  public diet_menu_type: any[];
+  public diet_week: any[];
+  public diet_day: any[];
+  public selectedOptions: any[] = [];
+  public selectedOptions2: any[] = [];
+  public diet_menu_dish: any[];
+
+  public dishByMenu: boolean = true;
+
+
+  constructor(
+    protected dialogRef: NbDialogRef<any>,
+    private dietMenuDishS: DietMenuDishService,
+  ) {
+  }
+
+  ngOnInit(): void {
+    if (!this.data) {
+      this.data = {
+        name: '',
+        diet_consistency_id: '',
+        diet_menu_type_id: '',
+        diet_week_id: '',
+        diet_day_id: '',
+      };
+    }
+
+    this.dietMenuDishS.GetCollection({ diet_menu_id: this.data.id }).then(x => {
+      this.diet_menu_dish = x;
+      x.forEach(element => {
+        this.selectedOptions2.push(element.diet_dish_id);
+      });
+    });
+
+
+  }
+
+  close() {
+    this.dialogRef.close();
+  }
+
+
+}
