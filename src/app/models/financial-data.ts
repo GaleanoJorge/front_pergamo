b4b4fd3d141b9fa66ea434892602dfcb99243135
Adriana Parra <gianalista@hlips.com.co>
2022-03-25 09:26:19 -0500

    tablas faltantes en terceros asistenciales

---
+++ src/app/models/financial-data.ts
@@ -0,0 +1,12 @@
+import { MainClass } from './main-class';
+import { BankInformation } from './bank-information';
+
+
+export class FinancialData extends MainClass {
+  id: number;
+  bank_information_id: BankInformation;
+  rut: string; 
+
+  
+
+}
