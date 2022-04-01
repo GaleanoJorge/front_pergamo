bacc1ef48e4f82febd91fb534bc225ef54035d7f
Daniel Montanez <gibasedatosydesarrollo@hlips.com.co>
2022-04-01 12:37:05 +0000

    path-complementary permissions deleted

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -75,8 +75,7 @@ const routes: Routes = [{
       path: 'pad-complementary',
       loadChildren: () => import('./pad-complementary/pad-complementary.module')
         .then(m => m.PadComplementaryModule),
-        canActivate: [PermissionsGuard],
-      data: { permission: 'gloss.read' }
+	 }
     },
     {
       path: '',
