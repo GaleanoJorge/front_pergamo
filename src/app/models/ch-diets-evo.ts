f2d8d06a2cd42929b8f8684693f003eb4443d127
Adriana Parra <gianalista@hlips.com.co>
2022-05-13 12:25:50 -0500

    Cambios realizados en Dietas

--- src/app/models/ch-diets-evo.ts
+++ src/app/models/ch-diets-evo.ts
@@ -2,7 +2,7 @@ import { MainClass } from './main-class';
 
 export class ChDietsEvo extends MainClass{
     id: number;
-    diet_component_id: number;
+    enterally_diet_id: number;
     diet_consistency_id: number;
     type_record_id: number;
     ch_record_id: number;
