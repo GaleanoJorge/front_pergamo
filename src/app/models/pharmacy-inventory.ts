1057dd48e5657a61fad130de5a22d5345909edc9
Harold <gidesarrolladorapp2@hlips.com.co>
2022-05-15 12:28:02 -0500

    FARMACIA ACTUALZIACION DE MEDICAMENTOS

--- src/app/models/pharmacy-inventory.ts
+++
@@ -1,9 +0,0 @@
-import { Time } from '@angular/common';
-import { SingleSeries } from '@swimlane/ngx-charts';
-import { MainClass } from './main-class';
-
-export class PharmacyInventory extends MainClass {
-  id: number;
-  pharmacy_stock_id: number;
-  pharmacy_lot_id: number;
-}
