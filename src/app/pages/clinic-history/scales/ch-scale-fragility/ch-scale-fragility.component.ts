2e64a81d7eaa38d1919f3a90bcf2ca1946170764
Ana M Lopez <annam.lopezb@gmail.com>
2022-05-25 13:46:43 -0500

    creación componente escala Fragility

---
+++ src/app/pages/clinic-history/scales/ch-scale-fragility/ch-scale-fragility.component.ts
@@ -0,0 +1,79 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { BaseTableComponent } from '../../../components/base-table/base-table.component';
+import { ActivatedRoute } from '@angular/router';
+import { NbDialogService, NbToastrService } from '@nebular/theme';
+import { DateFormatPipe } from '../../../../pipe/date-format.pipe';
+import { ChHistoricScalesComponent } from '../ch-historic-scales/ch-historic-scales.component';
+
+@Component({
+  selector: 'ngx-ch-scale-fragility',
+  templateUrl: './ch-scale-fragility.component.html',
+  styleUrls: ['./ch-scale-fragility.component.scss'],
+})
+export class ChScaleFragilidadComponent implements OnInit {
+  @ViewChild(BaseTableComponent) table: BaseTableComponent;
+  linearMode = true;
+  public messageError = null;
+  public title: string = 'Registros Escala Fragilidad';
+  public subtitle: string = '';
+  public headerFields: any[] = ['Fecha','Puntaje', 'Clasificación'];
+  public routes = [];
+  public data = [];
+  public ch_record_id;
+  public loading: boolean = false;
+  public saved: any = null;
+  public isSubmitted = false;
+
+  public settings = {
+    columns: {
+      created_at: {
+        title: this.headerFields[0],
+        type: 'string',
+        valuePrepareFunction: (value, row) => {
+          return this.datePipe.transform2(value);
+        },
+      },
+      total: {
+        title: this.headerFields[1],
+        type: 'string',
+      },
+      classification: {
+        title: this.headerFields[2],
+        type: 'string',
+      },
+    
+    },
+  };
+
+  constructor(
+    private dialogFormService: NbDialogService,
+    private route: ActivatedRoute,
+    private toastService: NbToastrService,
+    public datePipe: DateFormatPipe,
+
+  ) {
+  }
+  GetParams() {
+    return {
+      ch_record_id: this.route.snapshot.params.id,
+    };
+  }
+
+  ngOnInit(): void {
+    this.ch_record_id = this.route.snapshot.params.id;
+  }
+  Historic() {
+    this.dialogFormService.open(ChHistoricScalesComponent, {
+      context: {
+        title: 'Escala Fragilidad',
+        path: 'ch_scale_fragility',
+        catch_info: 'ch_scale_fragility',
+        ch_record_id: this.ch_record_id,
+      },
+    });
+  }
+
+  RefreshData() {
+    this.table.refresh();
+  }
+}
