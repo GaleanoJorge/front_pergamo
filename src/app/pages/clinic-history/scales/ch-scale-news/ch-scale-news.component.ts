ca747df9e0db49294f27d26624f91910a0e4dc5f
Ana M Lopez <annam.lopezb@gmail.com>
2022-05-25 14:23:50 -0500

    creación componente escala News

---
+++ src/app/pages/clinic-history/scales/ch-scale-news/ch-scale-news.component.ts
@@ -0,0 +1,89 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { BaseTableComponent } from '../../../components/base-table/base-table.component';
+import { ActivatedRoute } from '@angular/router';
+import { NbDialogService, NbToastrService } from '@nebular/theme';
+import { DateFormatPipe } from '../../../../pipe/date-format.pipe';
+import { ChHistoricScalesComponent } from '../ch-historic-scales/ch-historic-scales.component';
+
+@Component({
+  selector: 'ngx-ch-scale-news',
+  templateUrl: './ch-scale-news.component.html',
+  styleUrls: ['./ch-scale-news.component.scss'],
+})
+export class ChScaleNewsComponent implements OnInit {
+  @ViewChild(BaseTableComponent) table: BaseTableComponent;
+  linearMode = true;
+  public messageError = null;
+  public title: string = 'Registros Escala News';
+  public subtitle: string = '';
+  public headerFields: any[] = ['Fecha', 'Calificación', 'Riesgo Clínico', 'Respuesta clínica'];
+  public routes = [];
+  public data = [];
+  public ch_record_id;
+  public loading: boolean = false;
+  public saved: any = null;
+  public isSubmitted = false;
+
+  public settings = {
+    columns: {
+      created_at: {
+        title: this.headerFields[0],
+        type: 'string',
+        valuePrepareFunction: (value, row) => {
+          return this.datePipe.transform2(value);
+        },
+      },
+      qualification: {
+        title: this.headerFields[1],
+        type: 'string',
+      },
+      risk: {
+        title: this.headerFields[2],
+        type: 'string',
+      },
+      response: {
+        title: this.headerFields[3],
+        type: 'string',
+      }
+
+    },
+  };
+
+  constructor(
+    private dialogFormService: NbDialogService,
+    private route: ActivatedRoute,
+    private toastService: NbToastrService,
+    public datePipe: DateFormatPipe,
+
+  ) {
+  }
+  GetParams() {
+    return {
+      ch_record_id: this.route.snapshot.params.id,
+    };
+  }
+
+  ngOnInit(): void {
+    this.ch_record_id = this.route.snapshot.params.id;
+  }
+  Historic() {
+    this.dialogFormService.open(ChHistoricScalesComponent, {
+      context: {
+        title: 'Escala News',
+        path: 'ch_scale_news',
+        catch_info: 'ch_scale_news',
+        ch_record_id: this.ch_record_id,
+      },
+    });
+  }
+
+  RefreshData() {
+    this.table.refresh();
+  }
+
+  receiveMessage($event) {
+    if ($event == true) {
+      this.RefreshData();
+    }
+  }
+}
