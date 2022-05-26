e99b6990ebadedc2a801ec0c2f524ad54789b53f
Jorge Galeano <gidesarrolladorapp1@hlips.com.co>
2022-05-26 11:57:33 -0500

    tipo de autorización eliminado de plan de manejo

--- src/app/pages/pad/management-plan/form-management-plan/form-management-plan.component.ts
+++ src/app/pages/pad/management-plan/form-management-plan/form-management-plan.component.ts
@@ -25,7 +25,6 @@ export class FormManagementPlanComponent implements OnInit {
   @Input() medical: boolean = false;
   @Input() assigned: boolean;
   @Input() admissions_id: any = null;
-  @Input() type_auth: any = null;
 
   public form: FormGroup;
   public loading: boolean = false;
@@ -313,7 +312,6 @@ export class FormManagementPlanComponent implements OnInit {
           admissions_id: this.admissions_id,
           procedure_id: this.procedure_id,
           product_id: this.product_id,
-          type_auth: this.type_auth,
           assistance_id: selectes_assistance_id,
           locality_id: this.user.locality_id,
           phone_consult: this.phone_consult,
@@ -362,7 +360,6 @@ export class FormManagementPlanComponent implements OnInit {
           observation: this.form.controls.observation.value,
           number_doses: this.form.controls.number_doses.value,
           dosage_administer: this.form.controls.dosage_administer.value,
-          type_auth: this.type_auth,
     
         }).then(x => {
           this.toastService.success('', x.message);
@@ -401,7 +398,6 @@ export class FormManagementPlanComponent implements OnInit {
           observation: this.form.controls.observation.value,
           number_doses: this.form.controls.number_doses.value,
           dosage_administer: this.form.controls.dosage_administer.value,
-          type_auth: this.type_auth,
     
         }).then(x => {
           this.toastService.success('', x.message);
