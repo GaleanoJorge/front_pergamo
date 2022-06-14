9f4d1ebabc36f8e6d38e4b727a633daf07ee22dc
Ivan Sanchez <gidesarrollador@hlips.com.co>
2022-06-14 17:44:44 -0500

    modulo ch-record-list recibe id3 tipo de atención

--- src/app/pages/clinic-history/clinic-history-routing.module.ts
+++ src/app/pages/clinic-history/clinic-history-routing.module.ts
@@ -22,7 +22,7 @@ const routes: Routes = [{
       component: ClinicHistoryNursingListComponent,
     },
     {
-      path: 'ch-record-list/:id/:id2',
+      path: 'ch-record-list/:id/:id2/:id3',
       component: ChRecordListComponent,
     },
     {
