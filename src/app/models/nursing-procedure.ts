9f55e1809955c85af3d8586f544f84b7fbfe7d0e
Ivan Sanchez <gidesarrollador@hlips.com.co>
2022-06-14 17:47:56 -0500

    modelos de los modulos de parametrización

---
+++ src/app/models/nursing-procedure.ts
@@ -0,0 +1,8 @@
+import { MainClass } from './main-class';
+
+export class NursingProcedure extends MainClass {
+  id: number;
+  name: string;
+  description: string;
+
+}
---
+++ src/app/models/skin-status.ts
@@ -0,0 +1,6 @@
+import { MainClass } from './main-class';
+
+export class SkinStatus extends MainClass {
+  id: number;
+  name: string;
+}
