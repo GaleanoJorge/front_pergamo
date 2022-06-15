98fd03cec83d9d266f6d2eceba7b4f145b36171b
Ana M Lopez <annam.lopezb@gmail.com>
2022-06-15 15:26:38 -0500

    HC TR Se crea modelo Objectives

---
+++ src/app/models/ch-objectives-therapy.ts
@@ -0,0 +1,17 @@
+import { MainClass } from './main-class';
+
+export class ChObjectivesTherapy extends MainClass {
+    id: number;
+    strengthen:string;
+    promote:string;
+    title:string;
+    improve:string;
+    re_education:string;
+    hold:string;
+    check:string;
+    train:string;
+    headline:string;
+    look_out:string;
+    type_record_id: number;
+    ch_record_id: number;
+}
