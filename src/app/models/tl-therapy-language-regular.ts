f0abb6bd432e3fa8b18db21dfccef0bffa903285
Adriana Parra <gianalista@hlips.com.co>
2022-06-15 15:22:02 -0500

    Crud regular

---
+++ src/app/models/tl-therapy-language-regular.ts
@@ -0,0 +1,9 @@
+import { MainClass } from './main-class';
+
+export class TlTherapyLanguageRegular extends MainClass{
+    id: number;
+    tl_therapy_language_id: number;
+    status_patient: string;
+    type_record_id: number;
+    ch_record_id: number;
+}
