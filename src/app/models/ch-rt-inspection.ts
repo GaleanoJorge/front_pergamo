1877f21c22a02b3d57ea0401da484c72a1d3a0f5
Ana M Lopez <annam.lopezb@gmail.com>
2022-06-15 15:21:51 -0500

    HC TR Se crea modelo servicio componente Ispection

---
+++ src/app/models/ch-rt-inspection.ts
@@ -0,0 +1,13 @@
+import { MainClass } from './main-class';
+
+export class ChRtInspection extends MainClass {
+    id: number;
+    expansion:string;
+    masses:string;
+    crepitations:string;
+    fracturues:string;
+    airway:string;
+    pain:string;
+    type_record_id: number;
+    ch_record_id: number;
+}
