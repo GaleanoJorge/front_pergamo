c186f49ac1d4faa62b0fc0551bd19927b2539385
Fernando Castaño <rfcpuma09@gmail.com>
2022-06-21 09:35:09 -0500

    met

---
+++ src/app/models/ch-occupacional-therapy.ts
@@ -0,0 +1,9 @@
+import { MainClass } from './main-class';
+
+export class ChOccupacionalTherapy extends MainClass {
+    id: number;
+    ch_reason: string;
+    ch_diagnosis_id: number;
+    type_record_id: number;
+    ch_record_id: number;
+}
