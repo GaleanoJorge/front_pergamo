b793505cb7d281b2f1ce6a31eb77b7fab4d69130
Ivan Sanchez <gidesarrollador@hlips.com.co>
2022-07-05 10:03:05 -0500

    mod: desabilitar inputs de control de liquidos

--- src/app/pages/clinic-history/liquid-control/liquid-control.component.ts
+++ src/app/pages/clinic-history/liquid-control/liquid-control.component.ts
@@ -6,6 +6,9 @@ import { ActivatedRoute } from '@angular/router';
 import { NbToastrService } from '@nebular/theme';
 import { VALUE } from '@syncfusion/ej2-angular-filemanager';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { ChLiquidControlService } from '../../../business-controller/ch-liquid-control.service';
+import { nullSafeIsEquivalent } from '@angular/compiler/src/output/output_ast';
+import { time } from '@rxweb/reactive-form-validators';
 
 @Component({
   selector: 'ngx-liquid-control',
@@ -23,7 +26,7 @@ export class LiquidControlComponent implements OnInit {
   public messageError = null;
   public title: string = 'CONTROL DE FLUIDOS';
   public subtitle: string = 'INGRESADOS/ELIMINADOS';
-  public headerFields: any[] = ['FLUIDO', 'ELEMENTO', 'TIPO DE FLUIDO', 'CANTIDAD (CC)', 'ADICIONAL', 'HORA DEL EVENTO' ];
+  public headerFields: any[] = ['FLUIDO', 'ELEMENTO', 'TIPO DE FLUIDO', 'CANTIDAD (CC)', 'ADICIONAL', 'HORA DEL EVENTO'];
   public routes = [];
   public data = [];
   public auxForm: any = null;
@@ -31,6 +34,15 @@ export class LiquidControlComponent implements OnInit {
   public saved: any = null;
   public isSubmitted = false;
   public form: FormGroup;
+
+  public calulateLiquidControl: any[] = [];
+  public vital_signs: any[] = [];
+  public hour;
+  public urinalWaste;
+  public hidricBalance;
+  public Diur = 0;
+  public admin = 0;
+  public eliminate = 0;
 
 
   toggleLinearMode() {
@@ -52,8 +64,13 @@ export class LiquidControlComponent implements OnInit {
         type: 'string',
         valuePrepareFunction: (value, row) => {
           if (row.ch_route_fluid.type == 0) {
+            // this.admin = this.admin + row.delivered_volume;
             return 'INGRESA'
           } else {
+            // if(row.ch_route_fluid.name == "DIURESIS"){
+            //   this.Diur = this.Diur + row.delivered_volume;
+            // }
+            // this.admin = this.admin + row.delivered_volume;
             return 'ELIMINA'
           }
         },
@@ -89,7 +106,7 @@ export class LiquidControlComponent implements OnInit {
         },
       },
       clock: {
-        title: this.headerFields[3],
+        title: this.headerFields[5],
         type: 'string',
         valuePrepareFunction: (value, row) => {
           if (value) {
@@ -107,6 +124,8 @@ export class LiquidControlComponent implements OnInit {
     private route: ActivatedRoute,
     private toastService: NbToastrService,
     private formBuilder: FormBuilder,
+    private liquidControlS: ChLiquidControlService,
+    private chVitalSignS: ChVitalSignsService,
 
   ) {
 
@@ -126,7 +145,7 @@ export class LiquidControlComponent implements OnInit {
     };
   }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
     this.record_id = this.route.snapshot.params.id;
 
     if (!this.auxForm) {
@@ -137,14 +156,99 @@ export class LiquidControlComponent implements OnInit {
     }
 
     this.form = this.formBuilder.group({
-      urine_waste:[this.auxForm.urine_waste],
-      urine_balance:[this.auxForm.urine_waste]
+      urine_waste: [this.auxForm.urine_waste],
+      urine_balance: [this.auxForm.urine_waste]
     });
 
+    this.liquidCalculator();
+  }
+
+  async liquidCalculator() {
+    var diur = 0;
+    var admin = 0;
+    var eliminate = 0;
+    var adicional;
+    await this.liquidControlS.GetByRecord(this.record_id).then(x => {
+      if (x.length > 0) {
+        this.calulateLiquidControl = x;
+        this.calulateLiquidControl.forEach(element => {
+          if (element.ch_route_fluid.type == 0) {
+            admin = admin + element.delivered_volume;
+          } else {
+            if (element.ch_route_fluid.name == "DIURESIS") {
+              diur = diur + element.delivered_volume
+            }
+            eliminate = eliminate + element.delivered_volume;
+          }
+        });
+      } else {
+        this.calulateLiquidControl = null;
+        this.toastService.warning('', 'Se deben agregar registros para calcular datos hídricos')
+      }
+    });
+
+    await this.chVitalSignS.GetByRecord(this.record_id).then(x => {
+      if (x.length > 0) {
+        this.vital_signs = x;
+      } else {
+        this.vital_signs = null;
+        this.toastService.warning('', 'Se deben diligenciar los signos vitales para hídricos')
+      }
+    });
+    if (this.calulateLiquidControl && this.vital_signs) {
+      let Maxhour = Number(this.calulateLiquidControl[0].clock.substring(0, 1));
+      let Maxmin = Number(this.calulateLiquidControl[0].clock.substring(3, 4));
+
+      let Minhour = Number(this.calulateLiquidControl[this.calulateLiquidControl.length - 1].clock.substring(0, 1));
+      let Minmin = Number(this.calulateLiquidControl[this.calulateLiquidControl.length - 1].clock.substring(3, 4));
+
+      var hour = Maxhour - Minhour;
+      var min = Maxmin - Minmin;
+      if (hour > 0) {
+        if (min >= 0) {
+          this.hour = hour + (min / 60);
+        } else {
+          min = 60 + min;
+          this.hour = (hour - 1) + (min / 60);
+        }
+      } else {
+        if (min >= 0) {
+          this.hour = hour + (min / 60);
+        } else {
+          min = 60 + min;
+          this.hour = (hour - 1) + (min / 60);
+        }
+      }
+
+      //Gasto Urinario
+      this.urinalWaste = this.vital_signs[0].weight / this.hour / diur;
+      if(this.urinalWaste <= 0.3){
+        adicional = 'ANURIA';
+      } else if(this.urinalWaste > 0.3 && this.urinalWaste <= 0.7 ) {
+        adicional = 'OLIGURIA';
+      } else if (this.urinalWaste > 0.7 && this.urinalWaste <= 1 ) {
+        adicional = 'NORMAL';
+      } else {
+        adicional = 'pailander'
+      }
+
+      //Balance hídrico
+      this.hidricBalance = admin - eliminate;
+      this.form.patchValue({
+        urine_waste: this.urinalWaste == Infinity ? 'Faltan datos': this.urinalWaste.toFixed(3)+ ' : ' + adicional,
+        urine_balance:  this.hidricBalance,
+      });
+
+      // max.setHours(this.calulateLiquidControl[0].clock.substring(0, 1), this.calulateLiquidControl[0].clock.substring(3, 4));
+
+    } else {
+
+    }
+    this.table.refresh();
   }
 
   RefreshData() {
-    this.table.refresh();
+    this.liquidCalculator();
   }
 
   NewChVitalSigns() {
