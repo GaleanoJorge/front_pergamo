17f6402ce8cb79162b5376c57eedf3c1b11406dd
Ana M Lopez <annam.lopezb@gmail.com>
2022-07-08 14:37:58 -0500

    CH TR Ajuste AuscultacionTherapy

--- src/app/pages/clinic-history/auscultacion-therapy/form-auscultacion-therapy.component.ts
+++ src/app/pages/clinic-history/auscultacion-therapy/form-auscultacion-therapy.component.ts
@@ -15,7 +15,6 @@ export class FormaAuscultacionTherapyComponent implements OnInit {
   @Input() title: string;
   @Input() data: any = null;
   @Input() record_id: any = null;
-  @Output() messageEvent = new EventEmitter<any>();
 
   public form: FormGroup;
   public isSubmitted: boolean = false;
@@ -23,6 +22,7 @@ export class FormaAuscultacionTherapyComponent implements OnInit {
   public loading: boolean = false;
   public disabled: boolean = false;
   public showTable;
+  messageEvent: any;
  
 
 
@@ -38,18 +38,49 @@ export class FormaAuscultacionTherapyComponent implements OnInit {
     if (!this.data || this.data.length == 0) {
       this.data = {
         aus: [],
-        observation: '',
+        obs_murmur: '',
+        obs_crepits: '',
+        obs_rales: '',
+        obs_stridor: '',
+        obs_pleural: '',
+        obs_roncus: '',
+        obs_wheezing: '',
 
       };
     }
     this.form = this.formBuilder.group({
       aus: [this.data[0] ? this.data[0].aus : this.data.aus, Validators.compose([Validators.required])],
-      observation: [this.data[0] ? this.data[0].observation : this.data.observation,],
+      obs_murmur: [this.data[0] ? this.data[0].obs_murmur : this.data.obs_murmur,],
+      obs_crepits: [this.data[0] ? this.data[0].obs_crepits : this.data.obs_crepits,],
+      obs_rales: [this.data[0] ? this.data[0].obs_rales : this.data.obs_rales,],
+      obs_stridor: [this.data[0] ? this.data[0].obs_stridor : this.data.obs_stridor,],
+      obs_pleural: [this.data[0] ? this.data[0].obs_pleural : this.data.obs_pleural,],
+      obs_roncus: [this.data[0] ? this.data[0].obs_roncus : this.data.obs_roncus,],
+      obs_wheezing: [this.data[0] ? this.data[0].obs_wheezing : this.data.obs_wheezing,],
            
     });    
+    
+    if (this.data.obs_murmur != '') {
+      this.form.controls.obs_murmur.disable();
+      this.form.controls.obs_crepits.disable();
+      this.form.controls.obs_rales.disable();
+      this.form.controls.obs_stridor.disable();
+      this.form.controls.obs_pleural.disable();
+      this.form.controls.obs_roncus.disable();
+      this.form.controls.obs_wheezing.disable();
+      this.disabled = true;
+    } else {
+      this.form.controls.obs_murmur.enable();
+      this.form.controls.obs_crepits.enable();
+      this.form.controls.obs_rales.enable();
+      this.form.controls.obs_stridor.enable();
+      this.form.controls.obs_pleural.enable();
+      this.form.controls.obs_roncus.enable();
+      this.form.controls.obs_wheezing.enable();
+      this.disabled = false;
+    }
 
   }
-
 
   async save() {
     this.isSubmitted = true;
@@ -61,7 +92,13 @@ export class FormaAuscultacionTherapyComponent implements OnInit {
         await this.AuscultacionS.Update({
           id: this.data.id,
           aus: this.form.controls.aus.value,
-          observation: this.form.controls.observation.value,
+          obs_murmur: this.form.controls.obs_murmur.value,
+          obs_crepits: this.form.controls.obs_crepits.value,
+          obs_rales: this.form.controls.obs_rales.value,
+          obs_stridor: this.form.controls.obs_stridor.value,
+          obs_pleural: this.form.controls.obs_pleural.value,
+          obs_roncus: this.form.controls.obs_roncus.value,
+          obs_wheezing: this.form.controls.obs_wheezing.value,
           type_record_id: 1,
           ch_record_id: this.record_id,
 
@@ -77,25 +114,24 @@ export class FormaAuscultacionTherapyComponent implements OnInit {
       } else {
         await this.AuscultacionS.Save({
           aus: this.form.controls.aus.value,
-          observation: this.form.controls.observation.value,
+          obs_murmur: this.form.controls.obs_murmur.value,
+          obs_crepits: this.form.controls.obs_crepits.value,
+          obs_rales: this.form.controls.obs_rales.value,
+          obs_stridor: this.form.controls.obs_stridor.value,
+          obs_pleural: this.form.controls.obs_pleural.value,
+          obs_roncus: this.form.controls.obs_roncus.value,
+          obs_wheezing: this.form.controls.obs_wheezing.value,
           type_record_id: 1,
           ch_record_id: this.record_id,
         }).then(x => {
           this.toastService.success('', x.message);
           this.messageEvent.emit(true);
-          this.form.setValue({ aus: [], observation: ''});
+          this.form.setValue({ aus: [], obs_murmur: '', obs_crepits:'', obs_rales:'',obs_stridor:'', obs_pleural:'',
+          obs_roncus:'',obs_wheezing:''});
           if (this.saved) {
             this.saved();
           }
-        }).catch(x => {
-          if (this.form.controls.has_caregiver.value == true) {
-            this.isSubmitted = true;
-            this.loading = true;
-          } else {
-            this.isSubmitted = false;
-            this.loading = false;
-          }
-
+          
         });
       }
 
