f9b548974f162714a9eeaa1106ebdf09a6d1055f
Harold Cardenas <gidesarrolladorapp2@hlips.com.co>
2022-07-19 16:00:39 -0500

    provicional activos fijos

--- src/app/models/fixed-assets.ts
+++ src/app/models/fixed-assets.ts
@@ -11,7 +11,8 @@ export class FixedAssets extends MainClass {
   plaque: string;
   name: string;
   model: string;
-  amount: string;
+  amount_total: string;
+  actual_amount: string;
   mark: string;
   serial: string;
   description: string;
@@ -19,4 +20,38 @@ export class FixedAssets extends MainClass {
   color: string;
   fixed_condition_id: number;
   company_id: number;
+
+  calibration_certificate: string;
+  health_register: string;
+  warranty: string;
+  cv: string;
+  last_maintenance: string;
+  last_pame: string;
+  interventions_carriet: string;
+  type: string;
+  mobile_fixed: string;
+  clasification_risk_id: string;
+  biomedical_classification_id: string;
+  code_ecri: string;
+  form_acquisition: string;
+  date_adquisicion: string;
+  date_warranty: string;
+  useful_life: string;
+  cost: number;
+  maker: string;
+  phone_maker: string;
+  email_maker: string;
+  power_supply: string;
+  predominant_technology: string;
+  volt: string;
+  stream: string;
+  power: string;
+  frequency_rank: string;
+  temperature_rank: string;
+  humidity_rank: string;
+  manuals: string;
+  guide: string;
+  periodicity_frequency_id: string;
+  calibration_frequency_id: string;
+
 }
--- src/app/pages/setting/fixed-assets-requests/form-fixed-assets-requests/form-fixed-assets-requests.component.ts
+++ src/app/pages/setting/fixed-assets-requests/form-fixed-assets-requests/form-fixed-assets-requests.component.ts
@@ -2,9 +2,8 @@ import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NbDialogRef, NbToastrService } from '@nebular/theme';
 import { FixedAddService } from '../../../../business-controller/fixed-add.service';
-import { FixedAssetsService } from '../../../../business-controller/fixed-assets.service';
+import { FixedLoanService } from '../../../../business-controller/fixed-loan.service';
 import { FixedLocationCampusService } from '../../../../business-controller/fixed-location-campus.service';
-import { FixedTypeService } from '../../../../business-controller/fixed-type.service';
 import { UserBusinessService } from '../../../../business-controller/user-business.service';
 import { AuthService } from '../../../../services/auth.service';
 
@@ -15,7 +14,7 @@ import { AuthService } from '../../../../services/auth.service';
 })
 export class FormFixedAssetsRequestsComponent implements OnInit {
 
-  
+
   @Input() title: string;
   @Input() data: any = null;
   @Input() parentData: any;
@@ -29,6 +28,7 @@ export class FormFixedAssetsRequestsComponent implements OnInit {
   public showTable;
   public fixed_location_campus_id: any[];
   public responsible_user_id: any[];
+  public fixed_assets_id: any[];
   public user;
   public selectedOptions: any[] = [];
 
@@ -38,10 +38,11 @@ export class FormFixedAssetsRequestsComponent implements OnInit {
     private toastService: NbToastrService,
     protected dialogRef: NbDialogRef<any>,
     private authService: AuthService,
-    
+    private toastS: NbToastrService,
     private FixedLocationCampusS: FixedLocationCampusService,
     private UserRoleBusinessS: UserBusinessService,
     private FixedAddS: FixedAddService,
+    private FixedLoanS: FixedLoanService,
 
   ) {
   }
@@ -50,8 +51,6 @@ export class FormFixedAssetsRequestsComponent implements OnInit {
     this.user = this.authService.GetUser();
     this.parentData = {
       selectedOptions: [],
-      entity: 'fixed_assets',
-      customData: 'fixed_assets',
     };
     if (!this.data) {
       this.data = {
@@ -74,53 +73,103 @@ export class FormFixedAssetsRequestsComponent implements OnInit {
     });
   }
 
-  close() {
-    this.dialogRef.close();
-  }
-
   receiveMessage($event) {
     this.selectedOptions = $event;
+  }
+  close() {
+    this.dialogRef.close();
   }
 
   async save() {
     this.isSubmitted = true;
     if (!this.form.invalid) {
-      this.loading = true;
-      this.showTable = false;
+      var valid_values = true;
+      var total_sent = 1;
+      if (!this.selectedOptions || this.selectedOptions.length == 0) {
+        valid_values = false;
+        this.toastS.danger('Debe seleccionar al menos un medicamento', 'Error');
+      }
+      if (valid_values) {
+        this.loading = true;
 
-      if (this.data.id) {
-        await this.FixedAddS.Update({
-          id: this.data.id,
-          fixed_location_campus_id: this.form.controls.fixed_location_campus_id.value,
-          responsible_user_id: this.form.controls.responsible_user_id.value,
-          observation: this.form.controls.observation.value,
-          status: 'ENVIADO',
-        }).then(x => {
-          this.toastService.success('', x.message);
-          if (this.saved) {
-            this.saved();
-          }
-        }).catch(x => {
-          this.isSubmitted = false;
-          this.loading = false;
-        });
-      } else {
-        await this.FixedAddS.Save({
-          fixed_location_campus_id: this.form.controls.fixed_location_campus_id.value,
-          responsible_user_id: this.form.controls.responsible_user_id.value,
-          observation: this.form.controls.observation.value,
-          status: 'ENVIADO',
-        }).then(x => {
-          this.toastService.success('', x.message);
-          this.form.setValue({ fixed_location_campus_id: '', responsible_user_id: '', observation: '' });
-          if (this.saved) {
-            this.saved();
-          }
-        }).catch(x => {
-          this.isSubmitted = false;
-          this.loading = false;
-        });
+        if (this.data.id) {
+          this.FixedAddS.updateInventoryByLot({
+            fixed_assets_id: this.data.id,
+            fixed_location_campus_id: this.form.controls.fixed_location_campus_id.value,
+            responsible_user_id: this.form.controls.responsible_user_id.value,
+            request_amount : 1,
+            status: 'ENVIADO',
+            observation: this.form.controls.observation.value,
+            // fixed_assets_id: JSON.stringify(this.selectedOptions),
+          }).then(x => {
+            this.toastService.success('', x.message);
+            this.close();
+            var id = x.data.fixed_add.id;
+            var contador = 0;
+            var err = 0;
+            if (this.saved) {
+              this.saved();
+            }
+            this.FixedLoanS.Update({
+              fixed_add_id: id,
+              fixed_assets_id: JSON.stringify(this.selectedOptions),
+            }, id).then(x => {
+            }).catch(x => {
+              err++;
+            });
+            contador++;
+
+            if (contador > 0) {
+              this.toastS.success(null, 'Se actualizaron ' + contador + ' elementos');
+            } else if (err > 0) {
+              this.toastS.danger(null, 'No se actualizaron ' + contador + ' elementos');
+            }
+            this.selectedOptions = [];
+            if (this.saved) {
+              this.saved();
+            }
+          }).catch(x => {
+            this.isSubmitted = false;
+            this.loading = false;
+          });
+        } else {
+          await this.FixedAddS.updateInventoryByLot({
+            fixed_assets_id: this.data.id,
+            fixed_location_campus_id: this.form.controls.fixed_location_campus_id.value,
+            responsible_user_id: this.form.controls.responsible_user_id.value,
+            request_amount : 1,
+            status: 'ENVIADO',
+            observation: this.form.controls.observation.value,
+          }).then(x => {
+            this.toastService.success('', x.message);
+            this.close();
+            var id = x.data.fixed_add.id;
+            var contador = 0;
+            var err = 0;
+            this.FixedLoanS.Save({
+              fixed_add_id: id,
+              fixed_assets_id: JSON.stringify(this.selectedOptions),
+            }).then(x => {
+            }).catch(x => {
+              err++;
+            });
+            contador++;
+            if (contador > 0) {
+              this.toastS.success(null, 'Se actualizaron ' + contador + ' elementos');
+            } else if (err > 0) {
+              this.toastS.danger(null, 'No se actualizaron ' + contador + ' elementos');
+            }
+            this.selectedOptions = [];
+            if (this.saved) {
+              this.saved();
+            }
+          }).catch(x => {
+            this.isSubmitted = false;
+            this.loading = false;
+          });
+        }
       }
     }
   }
+
 }
