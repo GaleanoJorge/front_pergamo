3243c792c5ab70b8360d5e4bb974239519fb30e9
Adriana Parra <gianalista@hlips.com.co>
2022-07-22 10:34:59 -0500

    Ajuste de codigo ostomias

--- src/app/pages/clinic-history/ch-ostomies/form-ch-ostomies/form-ch-ostomies.component.ts
+++ src/app/pages/clinic-history/ch-ostomies/form-ch-ostomies/form-ch-ostomies.component.ts
@@ -1,12 +1,8 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { NbToastrService } from '@nebular/theme';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ChEvoSoapService } from '../../../../business-controller/ch-evo-soap.service';
 import { ChRecordService } from '../../../../business-controller/ch_record.service';
 import { ActivatedRoute } from '@angular/router';
-import { DiagnosisService } from '../../../../business-controller/diagnosis.service';
-import { InputMaterialsUsedTlService } from '../../../../business-controller/input-materials-used-tl.service';
-import { OstomiesTlService } from '../../../../business-controller/ostomies-tl.service';
 import { OstomyService } from '../../../../business-controller/ostomy.service';
 import { ChOstomiesService } from '../../../../business-controller/ch-ostomies.service';
 
@@ -29,10 +25,6 @@ export class FormChOstomiesComponent implements OnInit {
   public disabled: boolean = false;
   public showTable;
   public admissions_id;
-  public medical_diagnostic_id: any[]; 
-  public therapeutic_diagnosis_id: any[];
-  public diagnosis_id;
-  public diagnosis: any[];
   public ostomy_id: any[]=[];
  
 
