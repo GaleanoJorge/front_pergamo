39b21d06aff2a98678664d7d61aad0873dead29e
Harold Cardenas <gidesarrolladorapp2@hlips.com.co>
2022-07-28 19:07:13 -0500

    solicitud de factura cambio de posicion farmacia

---
+++ src/app/pages/pharmacy/billing-request-pharmacy/billing-request-pharmacy.component.ts
@@ -0,0 +1,119 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { BillingService } from '../../../business-controller/billing.service';
+import { NbDialogService } from '@nebular/theme';
+import { ConfirmDialogComponent } from '../../components/confirm-dialog/confirm-dialog.component';
+import { BaseTableComponent } from '../../components/base-table/base-table.component';
+import { FormBillingRequestPharmacyComponent } from './form-billing-request-pharmacy/form-billing-request-pharmacy.component';
+
+
+@Component({
+  selector: 'ngx-billing-request-pharmacy',
+  templateUrl: './billing-request-pharmacy.component.html',
+  styleUrls: ['./billing-request-pharmacy.component.scss']
+})
+export class BillingRequestPharmacyComponent implements OnInit {
+
+  public isSubmitted = false;
+  public messageError: string = null;
+  public title: string = 'Solicitudes a compras';
+  public subtitle: string = '';
+  public headerFields: any[] = ['Identificador', 'Producto solicitado', 'Cantidad'];
+  public messageToltip: string = `Búsqueda por: ${this.headerFields[0]}`;
+  public icon: string = 'nb-star';
+  public data = [];
+
+  @ViewChild(BaseTableComponent) table: BaseTableComponent;
+  public settings = {
+    pager: {
+      display: true,
+      perPage: 10,
+    },
+    columns: {
+      actions: {
+        title: 'Acciones',
+        type: 'custom',
+        valuePrepareFunction: (value, row) => {
+          // DATA FROM HERE GOES TO renderComponent
+          return {
+            'data': row,
+            'edit': this.EditBilling.bind(this),
+            'delete': this.DeleteConfirmBilling.bind(this),
+          };
+        },
+        // renderComponent: ActionsComponent,
+      },
+      id: {
+        title: this.headerFields[0],
+        type: 'string',
+      },
+      product_generic: {
+        title: this.headerFields[1],
+        type: 'string',
+        valuePrepareFunction: (value, row) => {
+          if (row.product_generic_id == null) {
+            return row.product_supplies.description;
+          } else {
+            return row.product_generic.description;
+          }
+        },
+      },
+      amount: {
+        title: this.headerFields[2],
+        type: 'string',
+      },
+    },
+  };
+
+  constructor(
+    private BillingS: BillingService,
+    private dialogFormService: NbDialogService,
+    private deleteConfirmService: NbDialogService,
+  ) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  RefreshData() {
+    this.table.refresh();
+  }
+
+  NewBilling() {
+    this.dialogFormService.open(FormBillingRequestPharmacyComponent, {
+      context: {
+        title: 'Crear solicitud de compra',
+        saved: this.RefreshData.bind(this),
+      },
+    });
+  }
+
+  EditBilling(data) {
+    this.dialogFormService.open(FormBillingRequestPharmacyComponent, {
+      context: {
+        title: 'Editar solicitud de compra',
+        data,
+        saved: this.RefreshData.bind(this),
+      },
+    });
+  }
+
+  DeleteConfirmBilling(data) {
+    this.deleteConfirmService.open(ConfirmDialogComponent, {
+      context: {
+        name: data.name,
+        data: data,
+        delete: this.DeleteBilling.bind(this),
+      },
+    });
+  }
+
+  DeleteBilling(data) {
+    return this.BillingS.Delete(data.id).then(x => {
+      this.table.refresh();
+      return Promise.resolve(x.message);
+    }).catch(x => {
+      throw x;
+    });
+  }
+
+}
---
+++ src/app/pages/pharmacy/billing-request-pharmacy/form-billing-request-pharmacy/form-billing-request-pharmacy.component.ts
@@ -0,0 +1,128 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { NbDialogRef, NbToastrService } from '@nebular/theme';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { TypeBillingEvidenceService } from '../../../../business-controller/type-billing-evidence.service';
+import { AuthService } from '../../../../services/auth.service';
+import { BillingStockRequestService } from '../../../../business-controller/billing-stock-request.service';
+import { BillingService } from '../../../../business-controller/billing.service';
+
+@Component({
+  selector: 'ngx-form-billing-request-pharmacy',
+  templateUrl: './form-billing-request-pharmacy.component.html',
+  styleUrls: ['./form-billing-request-pharmacy.component.scss']
+})
+export class FormBillingRequestPharmacyComponent implements OnInit {
+
+  @Input() title: string;
+  @Input() data: any = null;
+  @Input() parentData: any;
+  @Input() parentData1: any;
+
+  public form: FormGroup;
+  public isSubmitted: boolean = false;
+  public saved: any = null;
+  public loading: boolean = false;
+  public type_billing_evidence: any[];
+  public selectedOptions: any[] = [];
+  public selectedOptions1: any[] = [];
+  public user;
+
+  constructor(
+    protected dialogRef: NbDialogRef<any>,
+    private formBuilder: FormBuilder,
+    private BillingRequestS: BillingService,
+    private BillingStockRequestS: BillingStockRequestService,
+    private toastService: NbToastrService,
+    private typeEvidenS: TypeBillingEvidenceService,
+    private authService: AuthService,
+    private toastS: NbToastrService
+  ) {
+  }
+
+  async ngOnInit() {
+    this.user = this.authService.GetUser();
+    this.parentData = {
+      selectedOptions: [],
+      entity: 'product_generic',
+      customData: 'product_generic',
+    };
+
+    this.parentData1 = {
+      selectedOptions1: [],
+      entity: 'product_supplies',
+      customData: 'product_supplies',
+    };
+
+    if (!this.data) {
+      this.data = {
+        type_billing_evidence_id: '',
+      };
+    }
+    this.form = this.formBuilder.group({
+      type_billing_evidence_id: [this.data.type_billing_evidence_id, Validators.compose([Validators.required])],
+    });
+
+    await this.typeEvidenS.GetCollection({id: 2}).then(x => {
+      this.type_billing_evidence = x;
+    });
+  }
+  close() {
+    this.dialogRef.close();
+  }
+
+  receiveMessage($event) {
+    this.selectedOptions = $event;
+  }
+  receiveMessage1($event) {
+    this.selectedOptions1 = $event;
+  }
+
+  save() {
+    this.isSubmitted = true;
+    if (!this.form.invalid) {
+      this.loading = true;
+      this.BillingRequestS.Save({
+        company_id: null,
+        pharmacy_stock_id: null,
+        type_billing_evidence_id: this.form.controls.type_billing_evidence_id.value,
+      }).then(x => {
+        this.toastService.success('', x.message);
+        this.close();
+        var id = x.data.billing.id;
+        var contador = 0;
+        var err = 0;
+        if (this.saved) {
+          this.saved();
+        }
+        if (!this.selectedOptions.length) {
+          this.toastS.danger(null, 'Debe seleccionar al menos un medicamento');
+        }
+        else {
+          this.BillingStockRequestS.Save({
+            billing_id: id,
+            product_generic_id: this.selectedOptions || this.selectedOptions.length > 0 ? JSON.stringify(this.selectedOptions) : null,
+            product_supplies_id: this.selectedOptions1 || this.selectedOptions1.length > 0 ? JSON.stringify(this.selectedOptions1) : null,
+          }).then(x => {
+          }).catch(x => {
+            err++;
+          });
+          contador++;
+          if (contador > 0) {
+            this.toastS.success(null, 'Se actualizaron ' + contador + ' elementos');
+          } else if (err > 0) {
+            this.toastS.danger(null, 'No se actualizaron ' + contador + ' elementos');
+          }
+          this.selectedOptions = [];
+          this.selectedOptions1 = [];
+        }
+        if (this.saved) {
+          this.saved();
+        }
+      }).catch(x => {
+        this.isSubmitted = false;
+        this.loading = false;
+      });
+    }
+  }
+}
+
