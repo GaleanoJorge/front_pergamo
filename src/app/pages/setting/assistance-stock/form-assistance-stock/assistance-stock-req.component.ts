7c8d12035fd10838facd7202a214eb0c5813b835
Harold Cardenas <gidesarrolladorapp2@hlips.com.co>
2022-07-28 19:48:24 -0500

    cambio import a algo de ivan en farmacia

--- src/app/pages/setting/assistance-stock/form-assistance-stock/assistance-stock-req.component.ts
+++ src/app/pages/setting/assistance-stock/form-assistance-stock/assistance-stock-req.component.ts
@@ -7,8 +7,8 @@ import { FormAssistanceStockComponent } from './form-assistance-stock.component'
 import { PharmacyStockService } from '../../../../business-controller/pharmacy-stock.service';
 import { AuthService } from '../../../../services/auth.service'; 
 import { ActionsStockComponent } from '../actions.component';
-import { FormPharmacyIncomeComponent } from '../../pharmacy-income/form-pharmacy-income/form-pharmacy-income.component';
 import { PharmacyProductRequestService } from '../../../../business-controller/pharmacy-product-request.service';
+import { FormPharmacyIncomeComponent } from '../../../pharmacy/pharmacy-income/form-pharmacy-income/form-pharmacy-income.component';
 
 
 @Component({
