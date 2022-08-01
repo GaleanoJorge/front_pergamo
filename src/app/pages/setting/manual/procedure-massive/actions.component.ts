cbcfcac087821f81071d554fd1acaf832faa7c1c
Ivan Sanchez <gidesarrollador@hlips.com.co>
2022-08-01 16:08:35 -0500

    ver descripción responsiva

--- src/app/pages/setting/manual/procedure-massive/actions.component.ts
+++ src/app/pages/setting/manual/procedure-massive/actions.component.ts
@@ -1,5 +1,5 @@
-import {Component, Input} from '@angular/core';
-import {ViewCell} from 'ng2-smart-table';
+import { Component, Input } from '@angular/core';
+import { ViewCell } from 'ng2-smart-table';
 @Component({
   template: `
     <div class="d-flex justify-content-center">
@@ -15,18 +15,34 @@ import {ViewCell} from 'ng2-smart-table';
       <a *ngIf="value.data.manual_procedure_type_id==3"  nbTooltip="Información del paquete" nbTooltipPlacement="top" nbTooltipStatus="primary" nbButton ghost [routerLink]="'/pages/setting/manual/procedure-massive/procedure-package/' + value.data.id">
         <nb-icon icon="info-outline"></nb-icon>
       </a>
+      <button *ngIf="this.rowData.description" nbButton ghost [nbPopover]="templateRef"  nbPopoverTrigger="hover">
+        <nb-icon icon="arrowhead-up-outline"></nb-icon>
+      </button>
     </div>
+
+  <ng-template #templateRef>
+    <div class="p-3 container-fluid">
+      <nb-card style="max-width: 430px;max-height: 500px;overflow: auto;">
+      <nb-card-header>Descripción</nb-card-header>
+        <nb-card-body>
+          <p>
+            {{this.rowData.description}}     
+          </p>
+        </nb-card-body>
+      </nb-card>
+    </div>
+</ng-template>
     
   `,
 })
 export class ActionsComponentProcedure implements ViewCell {
   @Input() value: any;    // This hold the cell value
   @Input() rowData: any;  // This holds the entire row object
-  public ProcedurePackage:any[];
+  public ProcedurePackage: any[];
 
   constructor(
   ) {
   }
 
-    
+
 }
