728b4ef181cce99eae261dabfd2212e4e77ff3f7
Harold Cardenas <gidesarrolladorapp2@hlips.com.co>
2022-08-04 10:31:15 -0500

    MODELO FIXED STOCK

---
+++ src/app/models/fixed-stock.ts
@@ -0,0 +1,9 @@
+import { Time } from '@angular/common';
+import { SingleSeries } from '@swimlane/ngx-charts';
+import { MainClass } from './main-class';
+
+export class FixedStock extends MainClass {
+  id: number;
+  fixed_type_id: string;
+  campus_id: string;
+}
