fdc3cdcbdedc1d5509f0f3858425d9985a847f88
Harold Cardenas <gidesarrolladorapp2@hlips.com.co>
2022-08-29 12:06:19 -0500

    oculto mientras el envio de activos

--- src/app/pages/setting/fixed-inventary/actionsInFix.component.ts
+++ src/app/pages/setting/fixed-inventary/actionsInFix.component.ts
@@ -4,9 +4,9 @@ import {ViewCell} from 'ng2-smart-table';
 @Component({
   template: `
     <div class="d-flex justify-content-center">
-      <button nbTooltip="DESPACHO" nbTooltipPlacement="top" nbTooltipStatus="primary"  ngxCheckPerms="update" nbButton ghost (click)="value.edit(value.data)">
+   <!--   <button nbTooltip="DESPACHO" nbTooltipPlacement="top" nbTooltipStatus="primary"  ngxCheckPerms="update" nbButton ghost (click)="value.edit(value.data)">
         <nb-icon icon="arrowhead-up-outline"></nb-icon>
-      </button>
+      </button> -->
       <button nbTooltip="Eliminar" nbTooltipPlacement="top" nbTooltipStatus="primary" nbButton ngxCheckPerms="delete" ghost (click)="value.delete(value.data)">
         <nb-icon icon="trash-2-outline"></nb-icon>
       </button>
