3bdc9c2f00702ccdd786dd28359abbbb375c2523
Harold Cardenas <gidesarrolladorapp2@hlips.com.co>
2022-08-29 07:45:58 -0500

    no omstrar boton de devolver farmacia

--- src/app/pages/pharmacy/pharmacy-income/actions.component.ts
+++ src/app/pages/pharmacy/pharmacy-income/actions.component.ts
@@ -8,9 +8,9 @@ import { ViewCell } from 'ng2-smart-table';
         <nb-icon icon="arrowhead-up-outline"></nb-icon>
       </button>
 
-      <button nbTooltip="DEVOLVER" nbTooltipPlacement="top" nbTooltipStatus="primary" nbButton ghost (click)="value.returned(value.data)">
+      <!--  <button nbTooltip="DEVOLVER" nbTooltipPlacement="top" nbTooltipStatus="primary" nbButton ghost (click)="value.returned(value.data)">
         <nb-icon icon="flip-2-outline"></nb-icon>
-      </button>
+      </button> -->
 
       <!-- <button nbTooltip="ELIMINAR" nbTooltipPlacement="top" nbTooltipStatus="primary" nbButton ghost (click)="value.delete(value.data)">
         <nb-icon icon="trash-2-outline"></nb-icon>
