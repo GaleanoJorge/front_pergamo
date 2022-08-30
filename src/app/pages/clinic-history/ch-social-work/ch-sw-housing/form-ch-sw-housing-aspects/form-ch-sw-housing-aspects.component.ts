e92af0ed0b9e2847a171ea4ab301673530fdf8b9
Ana María <gi.desarrollador4@hlips.com.co>
2022-08-30 08:40:18 -0500

    CH SW Validación formulario ArmedConflict

--- src/app/pages/clinic-history/ch-social-work/ch-sw-housing/form-ch-sw-housing-aspects/form-ch-sw-housing-aspects.component.ts
+++ src/app/pages/clinic-history/ch-social-work/ch-sw-housing/form-ch-sw-housing-aspects/form-ch-sw-housing-aspects.component.ts
@@ -76,6 +76,8 @@ export class FormChSwHousingAspectsComponent implements OnInit {
     this.housingTypeS.GetCollection().then(x => {
       this.housingType = x;
     });
+
+    this.onChange();
   
   }
     
@@ -132,6 +134,23 @@ export class FormChSwHousingAspectsComponent implements OnInit {
     }else {
       this.toastService.warning('', "Debe diligenciar los campos obligatorios");
     }
+  } 
+
+  async onChange() {
+
+    this.form.get('ch_sw_housing_type_id').valueChanges.subscribe(val => {
+      if (val =! 1) {
+
+        this.form.controls.flat.clearValidators();
+
+        this.form.controls.flat.setErrors(null);
+
+      } else {
+        this.form.controls.flat.setValidators(Validators.compose([Validators.required]));
+        this.form.patchValue({ flat:''});
+
+      };
+    });
   }
 
   
