c90fa46603a4c3cfbf3f127f8f11e38a0142095f
Fernando Castaño <rfcpuma09@gmail.com>
2022-09-01 15:31:44 -0500

    cambios ok

--- src/app/pages/clinic-history/entry-clinic-history-nursing/entry-clinic-history-nursing.component.ts
+++ src/app/pages/clinic-history/entry-clinic-history-nursing/entry-clinic-history-nursing.component.ts
@@ -7,6 +7,11 @@ import { ChReasonConsultationService } from '../../../business-controller/ch-rea
 import { ChVitalSignsService } from '../../../business-controller/ch-vital-signs.service';
 import { ChDiagnosisService } from '../../../business-controller/ch-diagnosis.service';
 import { ChPhysicalExamService } from '../../../business-controller/ch_physical_exam.service';
+import { AuthService } from '../../../services/auth.service';
+import { ChRecordService } from '../../../business-controller/ch_record.service';
+import { Location } from '@angular/common';
+import { ConfirmDialogCHComponent } from '../clinic-history-list/confirm-dialog/confirm-dialog.component';
+import { NbDialogService, NbToastrService } from '@nebular/theme';
 
 
 @Component({
@@ -31,6 +36,14 @@ export class EntryClinicHistoryNursingComponent implements OnInit {
   public chdiagnosis: any[];
   public nameForm: String;
   public movieForm: String;
+  public own_user;
+  public user;
+  public int: 0;
+  public saved: any = null;
+  public currentRole: any;
+  public signatureImage: string;
+  public has_input: any = null; // ya existe registro de ingreso
+  public input_done: boolean = false; // ya se registró algo en el ingreso
 
 
   public record_id;
@@ -49,6 +62,11 @@ export class EntryClinicHistoryNursingComponent implements OnInit {
     private chvitalSignsS: ChVitalSignsService,
     private chdiagnosisS: ChDiagnosisService,
     public userChangeS: UserChangeService,
+    private authService: AuthService,
+    private chRecord: ChRecordService,
+    private location: Location,
+    private deleteConfirmService: NbDialogService,
+    private toastService: NbToastrService,
 
 
   ) {
@@ -57,6 +75,21 @@ export class EntryClinicHistoryNursingComponent implements OnInit {
 
   async ngOnInit() {
     this.record_id = this.route.snapshot.params.id;
+    this.own_user = this.authService.GetUser();
+    this.chRecord.GetCollection({
+      record_id: this.record_id
+    }).then(x => {
+      this.has_input = x[0]['has_input']; // se añade el resultado de la variable has_input
+      if (this.has_input == true) { // si tiene ingreso se pone como true la variable que valida si ya se realizó el registro de ingreso para dejar finalizar la HC
+        this.input_done = true;
+      }
+      this.user = x[0]['admissions']['patients'];
+    });
+    if (!this.data) {
+      this.data = {
+        ch_diagnosis_id: '',
+      };
+    }
 
 
   }
@@ -68,6 +101,77 @@ export class EntryClinicHistoryNursingComponent implements OnInit {
       await this.chreasonconsultS.Update({});
       await this.chvitalSignsS.Update({});
     }
+  }
+
+  public back(): void {
+    this.location.back();
+  }
+
+  close() {
+    if (this.input_done) { // validamos si se realizó ingreso para dejar terminal la HC, de lo contrario enviamos un mensaje de alerta 
+      this.deleteConfirmService.open(ConfirmDialogCHComponent, {
+        context: {
+          signature: true,
+          title: 'Finalizar registro.',
+          delete: this.finish.bind(this),
+          showImage: this.showImage.bind(this),
+          // save: this.saveSignature.bind(this),
+          textConfirm: 'Finalizar registro'
+        },
+      });
+    } else {
+      this.toastService.warning('Debe diligenciar el ingreso', 'AVISO')
+    }
+  }
+
+  showImage(data) {
+    this.int++;
+    if (this.int == 1) {
+      this.signatureImage = null;
+    } else {
+      this.signatureImage = data;
+
+    }
+  }
+  // async saveSignature() {
+  //   var formData = new FormData();
+  //   formData.append('firm_file', this.signatureImage);
+  //   console.log(this.signatureImage);
+  // }
+
+  async finish(firm) {
+    // if(this.signatureImage!=null){
+      var formData = new FormData();
+      formData.append('id', this.record_id,);
+      formData.append('status', 'CERRADO');
+      formData.append('user', this.user);
+      formData.append('role', this.currentRole);
+      formData.append('user_id', this.own_user.id);
+      formData.append('firm_file', this.signatureImage);
+      
+      try {
+
+        let response;
+        
+        response = await this.chRecord.UpdateCH(formData, this.record_id);
+        this.location.back();
+        this.toastService.success('', response.message);
+        //this.router.navigateByUrl('/pages/clinic-history/ch-record-list/1/2/1');
+        this.messageError = null;
+        if (this.saved) {
+          this.saved();
+        }
+      } catch (response) {
+        this.messageError = response;
+        this.isSubmitted = false;
+        this.loading = false;
+        throw new Error(response);
+      }
+    // }else{
+    //   this.toastService.danger('Debe diligenciar la firma');
+  
+    // }
+
   }
 
   saveMcEa() {
--- src/app/pages/clinic-history/regularlanguage-list/regularlanguage-list.component.ts
+++ src/app/pages/clinic-history/regularlanguage-list/regularlanguage-list.component.ts
@@ -9,6 +9,10 @@ import { ChVitalSignsService } from '../../../business-controller/ch-vital-signs
 import { UserChangeService } from '../../../business-controller/user-change.service';
 import { ChRecordService } from '../../../business-controller/ch_record.service';
 import { CifDiagnosisTlService } from '../../../business-controller/cif-diagnosis-tl.service';
+import { AuthService } from '../../../services/auth.service';
+import { Location } from '@angular/common';
+import { ConfirmDialogCHComponent } from '../clinic-history-list/confirm-dialog/confirm-dialog.component';
+import { NbDialogService, NbToastrService } from '@nebular/theme';
 
 @Component({
   selector: 'ngx-regularlanguage-list',
@@ -27,6 +31,8 @@ export class RegularLanguageListComponent implements OnInit {
   public title;
   public routes = [];
   public user_id;
+  public user;
+  public own_user;
   public cifdiagnosistl: any[];
   public physical: any[];
   public chvitsigns: any[];
@@ -34,10 +40,16 @@ export class RegularLanguageListComponent implements OnInit {
   public nameForm: String;
   public movieForm: String;
   public admissions_id;
+  public signatureImage: string;
+  public currentRole: any;
+  public saved: any = null;
+  public has_input: any = null; // ya existe registro de ingreso
+  public input_done: boolean = false; // ya se registró algo en el ingreso
  
 
 
   public record_id;
+  public int: 0;
   public isSubmitted: boolean = false;
   public form: FormGroup;
   public all_changes: any[];
@@ -48,12 +60,16 @@ export class RegularLanguageListComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private route: ActivatedRoute,
+    private authService: AuthService,
     private CifDiagnosisTlS: CifDiagnosisTlService,
     private chphysicalS: ChPhysicalExamService,
     private chvitalSignsS: ChVitalSignsService,
     private chdiagnosisS: ChDiagnosisService,
     public userChangeS: UserChangeService,
     private chRecord: ChRecordService,
+    private location: Location,
+    private deleteConfirmService: NbDialogService,
+    private toastService: NbToastrService,
     
 
   ) {
@@ -62,6 +78,21 @@ export class RegularLanguageListComponent implements OnInit {
 
   async ngOnInit() {
     this.record_id = this.route.snapshot.params.id;
+    this.own_user = this.authService.GetUser();
+    this.chRecord.GetCollection({
+      record_id: this.record_id
+    }).then(x => {
+      this.has_input = x[0]['has_input']; // se añade el resultado de la variable has_input
+      if (this.has_input == true) { // si tiene ingreso se pone como true la variable que valida si ya se realizó el registro de ingreso para dejar finalizar la HC
+        this.input_done = true;
+      }
+      this.user = x[0]['admissions']['patients'];
+    });
+    if (!this.data) {
+      this.data = {
+        ch_diagnosis_id: '',
+      };
+    }
 
     this.chRecord.GetCollection(this.record_id).then(x => {
       this.admissions_id=x;
@@ -110,6 +141,77 @@ export class RegularLanguageListComponent implements OnInit {
       await this.CifDiagnosisTlS.Update({});
       
     }
+  }
+
+  public back(): void {
+    this.location.back();
+  }
+
+  close() {
+    if (this.input_done) { // validamos si se realizó ingreso para dejar terminal la HC, de lo contrario enviamos un mensaje de alerta 
+      this.deleteConfirmService.open(ConfirmDialogCHComponent, {
+        context: {
+          signature: true,
+          title: 'Finalizar registro.',
+          delete: this.finish.bind(this),
+          showImage: this.showImage.bind(this),
+          // save: this.saveSignature.bind(this),
+          textConfirm: 'Finalizar registro'
+        },
+      });
+    } else {
+      this.toastService.warning('Debe diligenciar el ingreso', 'AVISO')
+    }
+  }
+
+  showImage(data) {
+    this.int++;
+    if (this.int == 1) {
+      this.signatureImage = null;
+    } else {
+      this.signatureImage = data;
+
+    }
+  }
+  // async saveSignature() {
+  //   var formData = new FormData();
+  //   formData.append('firm_file', this.signatureImage);
+  //   console.log(this.signatureImage);
+  // }
+
+  async finish(firm) {
+    // if(this.signatureImage!=null){
+      var formData = new FormData();
+      formData.append('id', this.record_id,);
+      formData.append('status', 'CERRADO');
+      formData.append('user', this.user);
+      formData.append('role', this.currentRole);
+      formData.append('user_id', this.own_user.id);
+      formData.append('firm_file', this.signatureImage);
+      
+      try {
+
+        let response;
+        
+        response = await this.chRecord.UpdateCH(formData, this.record_id);
+        this.location.back();
+        this.toastService.success('', response.message);
+        //this.router.navigateByUrl('/pages/clinic-history/ch-record-list/1/2/1');
+        this.messageError = null;
+        if (this.saved) {
+          this.saved();
+        }
+      } catch (response) {
+        this.messageError = response;
+        this.isSubmitted = false;
+        this.loading = false;
+        throw new Error(response);
+      }
+    // }else{
+    //   this.toastService.danger('Debe diligenciar la firma');
+  
+    // }
+
   }
 
   saveMcEa() {
