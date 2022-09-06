fa1b3818983ccede3128710891777ad905963448
Ana María <gi.desarrollador4@hlips.com.co>
2022-09-06 16:50:42 -0500

    se crea componenete SwDiagnosis

---
+++ src/app/pages/clinic-history/ch-social-work/ch-sw-diagnosis/sw-diagnosis.component.ts
@@ -0,0 +1,92 @@
+
+import { CurrencyPipe } from '@angular/common';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { UserChangeService } from '../../../../business-controller/user-change.service';
+import { DateFormatPipe } from '../../../../pipe/date-format.pipe';
+import { BaseTableComponent } from '../../../components/base-table/base-table.component';
+
+@Component({
+  selector: 'ngx-sw-diagnosis',
+  templateUrl: './sw-diagnosis.component.html',
+  styleUrls: ['./sw-diagnosis.component.scss'],
+})
+export class SwDiagnosisComponent implements OnInit {
+
+  @ViewChild(BaseTableComponent) table: BaseTableComponent;
+  @Input() record_id;
+  @Input() type_record: any = null;
+
+
+  linearMode = true;
+  public messageError = null;
+  public title: string = '';
+  public subtitle: string = '';
+  public headerFields: any[] = ['Fecha', 'Diagnóstico de ingreso', 'Diagnóstico',];
+  public routes = [];
+  public loading: boolean = false;
+  public saved: any = null;
+  public isSubmitted = false;
+  public all_changes: any[];
+  public saveEntry: any = 0;
+
+
+  public settings = {
+    pager: {
+      display: true,
+      perPage: 30,
+    },
+
+    columns: {
+      created_at: {
+        title: this.headerFields[0],
+        type: 'string',
+        valuePrepareFunction: (value, row) => {
+          return this.datePipe.transform2(value);
+        },
+      },
+      ch_diagnosis: {
+        title: this.headerFields[1],
+        width: 'string',
+        valuePrepareFunction(value, row) {
+          if (value) {
+            return value.diagnosis.name;
+          } else {
+            return 'NO APLICA'
+          }
+        }
+      },
+      sw_diagnosis: {
+        title: this.headerFields[2],
+        width: 'string',
+        valuePrepareFunction(value, row) {
+          if (value) {
+            return value;
+          } else {
+            return 'NO APLICA'
+          }
+        }
+      },
+    },
+  };
+
+
+  constructor(
+    public userChangeS: UserChangeService,
+    public datePipe: DateFormatPipe,
+    private currency: CurrencyPipe,
+  ) {
+  }
+
+  async ngOnInit() {
+  }
+
+  RefreshData() {
+    this.table.refresh();
+  }
+
+  receiveMessage($event) {
+    if ($event == true) {
+      this.RefreshData();
+    }
+  }
+}
