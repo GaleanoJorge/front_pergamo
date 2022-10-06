2ed99cbbd934eeb762cb8d24f7de619dee7dd0a5
Ana María <gi.desarrollador4@hlips.com.co>
2022-10-06 14:47:23 -0500

    Ajuste campo ordenes medicas

--- src/app/pages/clinic-history/ch-medical-orders/form-ch-medical-orders/form-ch-medical-orders.component.ts
+++ src/app/pages/clinic-history/ch-medical-orders/form-ch-medical-orders/form-ch-medical-orders.component.ts
@@ -4,10 +4,10 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ChEvoSoapService } from '../../../../business-controller/ch-evo-soap.service';
 import { ChRecordService } from '../../../../business-controller/ch_record.service';
 import { ActivatedRoute } from '@angular/router';
-import { ProductService } from '../../../../business-controller/product.service';
 import { ChMedicalOrdersService } from '../../../../business-controller/ch-medical-orders.service';
-import { ProcedureService } from '../../../../business-controller/procedure.service';
 import { FrequencyService } from '../../../../business-controller/frequency.service';
+import { ProcedureService } from '../../../../business-controller/procedure.service';
+import { ServicesBriefcaseService } from '../../../../business-controller/services-briefcase.service';
 
 @Component({
   selector: 'ngx-form-ch-medical-orders',
@@ -38,7 +38,8 @@ export class FormChMedicalOrdersComponent implements OnInit {
     private chRecord: ChRecordService,
     private route: ActivatedRoute,
     private FrequencyS: FrequencyService,
-    private ProductS: ProcedureService,
+    private procedureS: ProcedureService,
+    private serviceS: ServicesBriefcaseService,
     private ChMedicalOrdersS: ChMedicalOrdersService,
   ) { }
 
@@ -53,6 +54,7 @@ export class FormChMedicalOrdersComponent implements OnInit {
       this.data = {
         ambulatory_medical_order: '',
         procedure_id: '',
+        services_briefcase_id: '',
         amount: '',
         frequency_id: '',
         observations: '',
@@ -60,21 +62,21 @@ export class FormChMedicalOrdersComponent implements OnInit {
       };
     };
 
-    this.ProductS.GetCollection().then(x => {
-      this.procedure = x;
-    });
     this.FrequencyS.GetCollection().then(x => {
       this.frequency_id = x;
     });
 
+    
     this.form = this.formBuilder.group({
       ambulatory_medical_order: [this.data.ambulatory_medical_order],
       procedure_id: [this.data.procedure_id],
       amount: [this.data.amount],
       frequency_id: [this.data.frequency_id],
       observations: [this.data.observations],
-
+      
     });
+    
+    this.onChange();
   }
 
   clearForm() {
@@ -99,6 +101,7 @@ export class FormChMedicalOrdersComponent implements OnInit {
             id: this.data.id,
             ambulatory_medical_order: this.form.controls.ambulatory_medical_order.value ? 'Sí' : null,
             procedure_id: this.procedure_id,
+            services_briefcase_id: this.procedure_id,
             amount: this.form.controls.amount.value,
             frequency_id: this.form.controls.frequency_id.value,
             observations: this.form.controls.observations.value,
@@ -126,7 +129,8 @@ export class FormChMedicalOrdersComponent implements OnInit {
         await this.ChMedicalOrdersS
           .Save({
             ambulatory_medical_order: this.form.controls.ambulatory_medical_order.value ? 'Sí' : null,
-            procedure_id: this.procedure_id,
+            procedure_id: this.form.controls.ambulatory_medical_order.value ? this.procedure_id : null,
+            services_briefcase_id: this.form.controls.ambulatory_medical_order.value ? null : this.procedure_id,
             amount: this.form.controls.amount.value,
             frequency_id: this.form.controls.frequency_id.value,
             observations: this.form.controls.observations.value,
@@ -169,8 +173,33 @@ export class FormChMedicalOrdersComponent implements OnInit {
 
     } else {
       this.procedure_id = null;
-      this.toastService.warning('', 'Debe seleccionar un procedimiento de la lista');
       this.form.controls.procedure_id.setErrors({ 'incorrect': true });
     }
+    this.toastService.warning('', 'Debe seleccionar un procedimiento de la lista');
   }
+
+
+onChange() {
+
+  this.form.get('ambulatory_medical_order').valueChanges.subscribe(val => {
+    this.procedure_id = null;
+      this.form.controls.procedure_id.setErrors({ 'incorrect': true });
+    if (val == 1) {
+
+      this.procedureS.GetCollection().then(x => {
+        this.procedure = x;
+      });  
+     
+
+    } else {
+      this.serviceS.GetCollection().then(x => {
+        this.procedure = x;
+      });
+   
+
+    };
+  });
+
 }
+}
+
