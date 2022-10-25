ce507b572c58c467d779e6f061589f60ec05b6ae
Daniel Felipe <danielf.gt@outlook.com>
2022-10-25 15:55:36 -0500

    configuracion de pouchdb en login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,3 +1,4 @@
+import { data } from 'jquery';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
@@ -10,12 +11,15 @@ import { ItemRolePermissionBusinessService } from '../../business-controller/ite
 import { environment } from '../../../environments/environment';
 import { NbLoginComponent } from '@nebular/auth';
 import { HttpClient } from '@angular/common/http';
+import { User } from '../../models/user';
+import { DbPwaService } from '../../services/authPouch.service';
+import PouchDB from 'pouchdb-browser';
 
 
 @Component({
   selector: 'ngx-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
   sso: boolean;
@@ -25,13 +29,16 @@ export class LoginComponent implements OnInit {
   public isSubmitted2 = false;
   public resetPasswordForm = false;
   public messageError: string = null;
-  public campus: any[];
+  public campus:any[]=[];
+  public campus2: any;
   public environmentInt: boolean;
   public app_version: string;
   private endPointCAS = environment.cas;
   private serviceurl = environment.url_service;
   private type_login = environment.login_sso;
   public showPassword = false;
+  public user_db;
+
 
   public register: boolean = false;
 
@@ -46,37 +53,51 @@ export class LoginComponent implements OnInit {
     private route: ActivatedRoute,
     private itemBS: ItemRolePermissionBusinessService,
     private http: HttpClient,
-  ) {
-  }
+    private dbPwaService: DbPwaService,
+    
+  ) {    this.user_db = new PouchDB('login_db'); }
 
   ngOnInit(): void {
+
+ 
+
+   
 
     if (this.type_login == 'true') {
       this.sso = true;
       let path;
-      this.route.url.subscribe(values => {
+      this.route.url.subscribe((values) => {
         path = values[0].path;
       });
       if (path == 'logout') {
-
         this.authService.LogOut();
         const url = this.endPointCAS + 'logout';
         location.href = url;
-
       } else {
         this.authService.LogOut();
         let course_id = this.route.snapshot.queryParams.course_id;
         let register = this.route.snapshot.queryParams.register;
-        if (this.route.snapshot.queryParams.register && this.route.snapshot.queryParams.course_id) {
-
-          const url = this.endPointCAS + 'login?service=' + this.serviceurl + '/public/register/students?register=' + register + '%26course_id=' + course_id;
+        if (
+          this.route.snapshot.queryParams.register &&
+          this.route.snapshot.queryParams.course_id
+        ) {
+          const url =
+            this.endPointCAS +
+            'login?service=' +
+            this.serviceurl +
+            '/public/register/students?register=' +
+            register +
+            '%26course_id=' +
+            course_id;
           location.href = url;
         } else {
-
-          const url = this.endPointCAS + 'login?service=' + this.serviceurl + '/public/validate-login';
+          const url =
+            this.endPointCAS +
+            'login?service=' +
+            this.serviceurl +
+            '/public/validate-login';
           location.href = url;
         }
-
       }
     } else {
       this.sso = false;
@@ -84,29 +105,38 @@ export class LoginComponent implements OnInit {
       this.app_version = environment.app_version;
       this.authService.LogOut();
       this.loginForm = this.formBuilder.group({
-        username: ['', Validators.compose([Validators.required, Validators.minLength(5)])],
-        password: ['', Validators.compose([Validators.required, Validators.minLength(5)])],
+        username: [
+          '',
+          Validators.compose([Validators.required, Validators.minLength(5)]),
+        ],
+        password: [
+          '',
+          Validators.compose([Validators.required, Validators.minLength(5)]),
+        ],
       });
       this.resetForm = this.formBuilder.group({
-        username: ['', Validators.compose([Validators.required, Validators.minLength(5)])],
+        username: [
+          '',
+          Validators.compose([Validators.required, Validators.minLength(5)]),
+        ],
       });
 
       this.register = this.route.snapshot.queryParams.register;
-      this.route.url.subscribe(values => {
+      this.route.url.subscribe((values) => {
         if (values[1]) {
-          this.userBS.verifyUser(values[1]).then(
-            (response: any) => {
+          this.userBS
+            .verifyUser(values[1])
+            .then((response: any) => {
               if (!response.status) {
                 this.toastrService.danger('', response.message);
                 setTimeout('window.location.reload()', 0);
               } else {
                 this.toastrService.success('', response.message);
               }
-            }).catch(
-              error => {
-                this.toastrService.danger('', error);
-              },
-            );
+            })
+            .catch((error) => {
+              this.toastrService.danger('', error);
+            });
         }
       });
     }
@@ -123,29 +153,74 @@ export class LoginComponent implements OnInit {
     this.showPassword = !this.showPassword;
   }
 
-  login() {
+  async login() {
+    if(navigator.onLine){
     this.isSubmitted = true;
+  
     if (!this.loginForm.invalid) {
-      this.authService.Login(this.loginForm.controls.username.value, this.loginForm.controls.password.value).then(x => {
-        this.userBS.GetUser().then(async x => {
-          if (x && x.roles.length > 0) {
+      this.authService
+        .Login(
+          this.loginForm.controls.username.value,
+          this.loginForm.controls.password.value
+        )
+        .then((x) => {
+          this.userBS
+            .GetUser()
+            .then(async (x) => {
+              if (x && x.roles.length > 0) {
+               
+                this.authService.SaveUser(x);
+                this.campus = await this.userCampusBS.GetCollection(
+                  this.authService.GetUser().id,
+                  { status_id: 1 }
+                );
 
-            this.authService.SaveUser(x);
+                this.dbPwaService.saveUser(x,this.loginForm.controls.username.value, this.loginForm.controls.password.value,this.campus,"a",);
 
-            await this.itemBS.GetCollection(this.authService.GetRole());
 
-            this.campus = await this.userCampusBS.GetCollection(this.authService.GetUser().id, {status_id: 1});
+                await this.itemBS.GetCollection(this.authService.GetRole(),this.loginForm.controls.username.value);
 
-          } else {
-            this.messageError = 'No tiene ningún rol asociado';
-          }
-        }).catch(x => {
+              } else {
+                this.messageError = 'No tiene ningún rol asociado';
+              }
+            })
+            .catch((x) => {
+              this.messageError = null;
+            });
+        })
+        .catch((x) => {
           this.messageError = null;
         });
-      }).catch(x => {
-        this.messageError = null;
-      });
     }
+  }
+  
+  else{
+    let user= this.loginForm.controls.username.value;
+    let pass= this.loginForm.controls.password.value;
+
+//  this.campus2=this.dbPwaService.returnCampus(user);
+
+//  console.log(this.campus2);
+
+    this.user_db.get(user).then(x =>  {
+      if(x.user==user && x.pass==pass){
+   
+        this.campus=x.campus;
+        this.authService.SaveUser(x.userAll);
+        localStorage.setItem(
+          'permissions',
+          JSON.stringify(x.permission)
+        );
+        return Promise.resolve(true);
+       
+      }
+      });
+
+      await this.itemBS.GetCollection(this.authService.GetRole(),user);
+     
+  
+  }
+  
   }
 
   async chargeCampus() {
@@ -169,12 +244,18 @@ export class LoginComponent implements OnInit {
     this.isSubmitted2 = true;
     if (!this.resetForm.invalid) {
       this.toastrService.success('', 'Restableciendo contraseña...');
-      this.authService.ResetPassword(this.resetForm.controls.username.value).then().catch(x => {
-        if (x == 'Se ha enviado un correo electrónico para que pueda restablecer la contraseña')
-          this.toastrService.success('', x);
-        this.resetPasswordForm = false;
-        this.messageError = null;
-      });
+      this.authService
+        .ResetPassword(this.resetForm.controls.username.value)
+        .then()
+        .catch((x) => {
+          if (
+            x ==
+            'Se ha enviado un correo electrónico para que pueda restablecer la contraseña'
+          )
+            this.toastrService.success('', x);
+          this.resetPasswordForm = false;
+          this.messageError = null;
+        });
     }
   }
 }
