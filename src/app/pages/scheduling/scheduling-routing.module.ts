e9b5827a8cc52bdb1be5341b1a04428ccbb8b137
Ivan Sanchez <gi.desarrollador3@hlips.com.co>
2022-11-08 10:48:13 -0500

    mod:add rutas para componentes agendamiento

--- src/app/pages/scheduling/scheduling-routing.module.ts
+++ src/app/pages/scheduling/scheduling-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AssistencialViewComponent } from './assistencial-view/assistencial-view.component';
+import { CopayCategoryComponent } from './copay_category/copay-category.component';
+import { HealthcareItineraryComponent } from './healtcare-itinerary/healthcare-itinerary.component';
 import { MedicalDiaryComponent } from './medical-diary/medical-diary.component';
 import { MedicalComponent } from './medical-diary/medical/medical.component';
 import { NonWorkingDaysComponent } from './non-working-days/non-working-days.component';
+import { ReasonCancelComponent } from './reason-cancel/reason-cancel.component';
 import { SchedulingComponent } from './scheduling.component';
 import { TelemedicineListComponent } from './telemedicine/telemedicine-list/telemedicine-list.component';
 
@@ -26,6 +30,22 @@ const routes: Routes = [
       {
         path: 'medical-diary/medical/:id/:user',
         component: MedicalComponent,
+      },
+      {
+        path: 'healthcare-itinerary',
+        component: HealthcareItineraryComponent,
+      },
+      {
+        path: 'copay-category',
+        component: CopayCategoryComponent,
+      },
+      {
+        path: 'assitencial-view',
+        component: AssistencialViewComponent,
+      },
+      {
+        path: 'reason-cancel',
+        component: ReasonCancelComponent,
       },
     ],
   },
