05203320c8c9845b3c2e471c872fa7eb64fe5ac5
Ivan Sanchez <gi.desarrollador3@hlips.com.co>
2022-11-08 10:27:34 -0500

    mod: ajuste form crear copagos

--- src/app/pages/scheduling/copay_category/form-copay_category/form-copay_category.component.ts
+++ src/app/pages/scheduling/copay_category/form-copay_category/form-copay_category.component.ts
@@ -5,8 +5,10 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NonWorkingDaysService } from '../../../../business-controller/non-working-days.service';
 import { TypeContractService } from '../../../../business-controller/type-contract.service';
 import { CopayParametersService } from '../../../../business-controller/copay-parameters.service';
-import { CurrencyPipe } from '@angular/common';
+import { CurrencyPipe, PercentPipe  } from '@angular/common';
 import { StatusBusinessService } from '../../../../business-controller/status-business.service';
+import { disable } from '@rxweb/reactive-form-validators';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'ngx-form-copay_category',
@@ -35,13 +37,14 @@ export class FormCopayCategoryComponent implements OnInit {
     private typeContractS: TypeContractService,
     private StatusS: StatusBusinessService,
     private currency: CurrencyPipe,
+    private percent: PercentPipe,
     private CopayParametersS: CopayParametersService
   ) {}
 
   ngOnInit(): void {
     if (!this.data) {
       this.data = {
-        type_contract_id: '',
+        payment_type: '',
         category: '',
         value: '',
         status_id: '',
@@ -49,39 +52,83 @@ export class FormCopayCategoryComponent implements OnInit {
     }
 
     this.form = this.formBuilder.group({
-      type_contract_id: [
-        this.data.type_contract_id,
+      payment_type: [
+        String(this.data.payment_type),
         Validators.compose([Validators.required]),
       ],
-      category: [this.data.category, Validators.compose([Validators.required])],
-      value: [this.data.value, Validators.compose([Validators.required])],
+      category: [
+        {
+          value: this.data.category,
+          disabled: this.data.payment_type == 3 ? true : false,
+        },
+        Validators.compose([Validators.required]),
+      ],
+      value: [
+        {
+          value: this.data.value,
+          disabled: this.data.payment_type == 3 ? true : false,
+        },
+        Validators.compose([Validators.required]),
+      ],
       status_id: [
-        this.data.status_id == '' ? 1 : this.data.status_id ,
+        this.data.status_id == '' ? 1 : this.data.status_id,
         Validators.compose([Validators.required]),
       ],
-    });
-
-    this.typeContractS.GetCollection().then((x) => {
-      this.type_contracts = x;
     });
 
     this.StatusS.GetCollection().then((x) => {
       this.status = x;
     });
+
+    this.onChanges();
   }
 
-  Moneyformat(e){
+  onChanges() {
+    this.form.get('payment_type').valueChanges.subscribe((val) => {
+      if (val == 3) {
+        this.form.get('category').disable();
+        this.form.get('value').disable();
+        this.form.patchValue({
+          category: 'EXENTO',
+          value: 0,
+        });
+      } else {
+        this.form.get('category').enable();
+        this.form.get('value').enable();
+        this.form.patchValue({
+          category: '',
+          value: '',
+        });
+      }
+    });
+
+    // this.form.get('value').valueChanges.subscribe(val => {
+    //   var num = Number(val);
+    //   if (num != NaN) {
+    //     this.value = num;
+    //     this.form.patchValue({
+    //       value: this.form.controls.payment_type.value == 1 ? this.currency.transform(num) : this.percent.transform(num),
+    //     });
+    //   } else {
+    //     this.value = undefined;
+    //     this.form.controls.value.setErrors({ incorrect: true });
+    //   }
+    // });
+  }
+
+  Moneyformat(e) {
     console.log(e);
-    var num = Number(e.target.value);
-    if(num != NaN){
-      this.value = num;
+    var num = Number(e);
+    if (num != NaN) {
+      this.value = this.form.controls.payment_type.value == 2 ? num/100 : num;
       this.form.patchValue({
-        value: this.currency.transform(num),
+          value: this.form.controls.payment_type.value == 2 ?  this.percent.transform(this.value) : this.currency.transform(num),
       });
-    }else{
+    } else {
       this.value = undefined;
-      this.form.controls.value.setErrors({incorrect: true});
+      this.form.controls.value.setErrors({ incorrect: true });
     }
+    return this.value;
   }
 
   close() {
@@ -94,7 +141,7 @@ export class FormCopayCategoryComponent implements OnInit {
       if (this.data.id) {
         this.CopayParametersS.Update({
           id: this.data.id,
-          type_contract_id: this.form.controls.type_contract_id.value,
+          payment_type: this.form.controls.payment_type.value,
           category: this.form.controls.category.value,
           value: this.value,
           status_id: this.form.controls.status_id.value,
@@ -113,11 +160,10 @@ export class FormCopayCategoryComponent implements OnInit {
           });
       } else {
         this.CopayParametersS.Save({
-          type_contract_id: this.form.controls.type_contract_id.value,
+          payment_type: this.form.controls.payment_type.value,
           category: this.form.controls.category.value,
-          value: this.value,
+          value: this.value ? this.value : 0,
           status_id: this.form.controls.status_id.value,
-
         })
           .then((x) => {
             this.toastService.success('', x.message);
