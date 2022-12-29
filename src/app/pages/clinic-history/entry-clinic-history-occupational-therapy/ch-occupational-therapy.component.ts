ca646ca8a14fbff02a8dbf7bccbc3a96d876048c
Ana María <gi.desarrollador4@hlips.com.co>
2022-12-29 17:21:51 -0500

    Ajuste texto encabezado

--- src/app/pages/clinic-history/entry-clinic-history-occupational-therapy/ch-occupational-therapy.component.ts
+++ src/app/pages/clinic-history/entry-clinic-history-occupational-therapy/ch-occupational-therapy.component.ts
@@ -270,11 +270,11 @@ export class ClinicHistoryOccupationalTherapy implements OnInit {
 
   tablock(e) {
     switch (e.tabTitle) {
-      case "INGRESO 1": {
+      case "INGRESO PARTE 1": {
         this.show = 1;
         break;
       }
-      case "INGRESO 2": {
+      case "INGRESO PARTE 2": {
         this.show = 2;
         break;
       }
