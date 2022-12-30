46187d009adf86f535c767fd98f4cc0924496074
Miguel Manrique <gi.desarrollador3@hlips.com>
2022-12-30 12:15:11 -0500

    Se hicieron ajustes de funcionalidad y diseño al componente de selección múltiple de fechas.

--- src/app/pages/components/multicalendar/multicalendar.component.ts
+++ src/app/pages/components/multicalendar/multicalendar.component.ts
@@ -28,18 +28,18 @@ export class MulticalendarComponent implements OnInit {
 
   private dictionaryMonth =
     [
-      ["Enero", 0],
-      ["Febrero", 1],
-      ["Marzo", 2],
-      ["Abril", 3],
-      ["Mayo", 4],
-      ["Junio", 5],
-      ["Julio", 6],
-      ["Agosto", 7],
-      ["Septiembre", 8],
-      ["Octubre", 9],
-      ["Noviembre", 10],
-      ["Diciembre", 11]
+      ["Ene", 0],
+      ["Feb", 1],
+      ["Mar", 2],
+      ["Abr", 3],
+      ["May", 4],
+      ["Jun", 5],
+      ["Jul", 6],
+      ["Ago", 7],
+      ["Sep", 8],
+      ["Oct", 9],
+      ["Nov", 10],
+      ["Dic", 11]
     ];
 
   constructor() {
@@ -60,54 +60,47 @@ export class MulticalendarComponent implements OnInit {
 
     let firstDay = (new Date(year, month)).getDay();
 
-    // body of the calendar
     var tbl = document.querySelector("#calendarBody");
-    // clearing all previous cells
+
     tbl.innerHTML = "";
 
-
     var monthAndYear = document.getElementById("monthAndYear");
-    // filing data about month and in the page via DOM.
-    monthAndYear.innerHTML = this.months[month] + " " + year;
 
+    monthAndYear.innerHTML = this.months[month] + " " + year;
 
     this.selectYear.value = year;
     this.selectMonth.value = month;
 
-    // creating the date cells here
     let date = 1;
 
-    // add the selected dates here to preselect
-    //selectedDates.push((month + 1).toString() + '/' + date.toString() + '/' + year.toString());
-
-    // there will be maximum 6 rows for any month
     for (let rowIterator = 0; rowIterator < 6; rowIterator++) {
 
-      // creates a new table row and adds it to the table body
+
       let row = document.createElement("tr");
 
-      //creating individual cells, filing them up with data.
+
       for (let cellIterated = 0; cellIterated < 7 && date <= this.daysInMonth(month, year); cellIterated++) {
 
-        // create a table data cell
+
         let cell = document.createElement("td");
         let textNode = "";
-        // check if this is the valid date for the month
+
         if (rowIterator !== 0 || cellIterated >= firstDay) {
           cell.id = (month + 1).toString() + '/' + date.toString() + '/' + year.toString();
-          //cell.class = "clickable";
+          let dateListFormat = year.toString() + '/' + (((month + 1).toString().length == 1) ? '0' + (month + 1).toString() : (month + 1).toString()) + '/' + ((date.toString().length == 1) ? '0' + date.toString() : date.toString())
+          if (this.selectedDates.includes(dateListFormat)) {
+            cell.classList.add("highlight");
+            cell.style.backgroundColor = "lightblue";
+          }
+
+
           textNode = date.toString();
 
-          // this means that highlightToday is set to true and the date being iterated it todays date,
-          // in such a scenario we will give it a background color
           if (this.highlightToday
             && date === this.today.getDate() && year === this.today.getFullYear() && month === this.today.getMonth()) {
             cell.classList.add("today-color");
           }
 
-          // set the previous dates to be selected
-          // if the selectedDates array has the dates, it means they were selected earlier. 
-          // add the background to it.
           if (this.selectedDates.indexOf((month + 1).toString() + '/' + date.toString() + '/' + year.toString()) >= 0) {
             cell.classList.add(this.highlightClass);
           }
@@ -119,13 +112,10 @@ export class MulticalendarComponent implements OnInit {
         cell.appendChild(cellText);
         row.appendChild(cell);
       }
-      tbl.appendChild(row); // appending each row into calendar body.
+      tbl.appendChild(row);
     }
-
-    // this adds the button panel at the bottom of the calendar
     this.addButtonPanel(tbl);
 
-    // function when the date cells are clicked
     let daysReturn = document.querySelectorAll("#calendarBody > tr > td");
     let multicalendarObject = this;
     for (var i = 0; i < daysReturn.length; i++) {
@@ -133,21 +123,21 @@ export class MulticalendarComponent implements OnInit {
       daysReturn[i].addEventListener('click', function (event) {
         this.setAttribute('id', i);
         var id = this.getAttribute('id');
-        // check the if cell clicked has a date
-        // those with an id, have the date
         let day = this.innerHTML;
         let selectedDate = new Date(year, month, day);
         let dateContent = selectedDate.getFullYear() + '/' +
           ((selectedDate.getMonth() + 1).toString().length == 1 ? ('0' + (selectedDate.getMonth() + 1).toString()) : (selectedDate.getMonth() + 1).toString())
-        + '/' +
+          + '/' +
           ((selectedDate.getDate()).toString().length == 1 ? ('0' + (selectedDate.getDate()).toString()) : (selectedDate.getDate()).toString());
         if (typeof id !== typeof undefined) {
           if (this.classList.length == 0) {
             this.setAttribute('class', i + "class");
           }
           if (!this.innerHTML.includes("Reset") && !this.innerHTML.includes("Done") && !this.getAttribute("class").includes("highlight")) {
-            multicalendarObject.selectedDates.push(dateContent);
-            multicalendarObject.messageEmit.emit(multicalendarObject.selectedDates);
+            if (!multicalendarObject.selectedDates.includes(dateContent)) {
+              multicalendarObject.selectedDates.push(dateContent);
+              multicalendarObject.messageEmit.emit(multicalendarObject.selectedDates);
+            }
           }
           else {
             var index = multicalendarObject.selectedDates.indexOf(dateContent);
@@ -167,12 +157,10 @@ export class MulticalendarComponent implements OnInit {
           }
         }
 
-        // sort the selected dates array based on the latest date first
         var sortedArray = multicalendarObject.selectedDates.sort((a, b) => {
           return new Date(a).getTime() - new Date(b).getTime();
         });
 
-        // update the selectedValues text input
         (document.getElementById('selectedValues') as HTMLInputElement).value = multicalendarObject.datesToString(sortedArray);
 
       });
@@ -182,10 +170,10 @@ export class MulticalendarComponent implements OnInit {
     let $dropBox: HTMLElement = document.querySelector('#parent');
 
     $search.addEventListener('blur', function (event) {
-      //$dropBox.hide();
+
     });
     $search.addEventListener('focus', function () {
-      //$dropBox.css( "display", "block" );
+
       $dropBox.style.display = "block";
     });
   }
@@ -240,7 +228,7 @@ export class MulticalendarComponent implements OnInit {
       this.years.push(year);
       select.options[select.options.length] = new Option(year.toString(), year.toString(), parseInt(year.toString()) === parseInt(selectedYear));
     }
-    //select.value = 2012;
+
   }
 
   daysInMonth(iMonth, iYear) {
@@ -248,7 +236,7 @@ export class MulticalendarComponent implements OnInit {
   }
 
   resetCalendar() {
-    // reset all the selected dates
+
     this.selectedDates = [];
     this.messageEmit.emit(this.selectedDates);
     let tr = document.querySelectorAll('#calendarBody  > tr');
@@ -262,13 +250,13 @@ export class MulticalendarComponent implements OnInit {
   };
 
   endSelection() {
-    //document.querySelector('#parent').hide();
+
     (document.querySelector('#parent') as HTMLDivElement).style.display = "none";
   }
 
   addButtonPanel(tbl) {
-    // after we have looped for all the days and the calendar is complete,
-    // we will add a panel that will show the buttons, reset and done
+
+
     let row = document.createElement("tr");
     row.className = 'buttonPanel';
     let cell = document.createElement("td");
@@ -310,7 +298,7 @@ export class MulticalendarComponent implements OnInit {
 
     cell.appendChild(parentDiv);
     row.appendChild(cell);
-    // appending each row into calendar body.
+
     tbl.appendChild(row);
   }
 
