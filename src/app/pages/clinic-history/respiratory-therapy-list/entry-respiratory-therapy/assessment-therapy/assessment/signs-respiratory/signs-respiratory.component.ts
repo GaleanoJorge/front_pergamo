fc64ac7b02963bd391e1fc9fd1e0b8bdb5dbd9a5
Ana María <gi.desarrollador4@hlips.com.co>
2023-02-10 13:32:22 -0500

    Organización carpetas HC Respiratoria

---
+++ src/app/pages/clinic-history/respiratory-therapy-list/entry-respiratory-therapy/assessment-therapy/assessment/signs-respiratory/signs-respiratory.component.ts
@@ -0,0 +1,87 @@
+import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { UserChangeService } from '../../../../../../../business-controller/user-change.service';
+import { DateFormatPipe } from '../../../../../../../pipe/date-format.pipe';
+import { BaseTableComponent } from '../../../../../../components/base-table/base-table.component';
+
+@Component({
+  selector: 'ngx-signs-respiratory',
+  templateUrl: './signs-respiratory.component.html',
+  styleUrls: ['./signs-respiratory.component.scss'],
+})
+export class SignsRespiratoryComponent implements OnInit {
+  @ViewChild(BaseTableComponent) table: BaseTableComponent;
+  @Input() record_id;
+  @Input() type_record: any = null;
+  @Input() type_record_id: any = null;
+  @Output() messageEvent = new EventEmitter<any>();
+
+  linearMode = true;
+  public messageError = null;
+  public title: string = '';
+  public subtitle: string = '';
+  public headerFields: any[] = ['Fecha', 'Signos de dificltad respiratoria' ];
+  public routes = [];
+  public data = [];
+  public loading: boolean = false;
+  public saved: any = null;
+  public isSubmitted = false;
+  public all_changes: any[];
+  public saveEntry: any = 0;
+
+
+  public settings = {
+    pager: {
+      display: true,
+      perPage: 30,
+    },
+    
+    columns: {
+      created_at: {
+        title: this.headerFields[0],
+        type: 'string',
+        valuePrepareFunction: (value) => {
+          return this.datePipe.transform4(value);
+        },
+      },
+      fluter: {
+        title: this.headerFields[1],
+        width: 'string',
+        valuePrepareFunction: (value, row) => {
+          return (row.fluter != null ? row.fluter  + ' - ' : "") 
+          + (row.distal != null ? row.distal  + ' - ' : "")
+          + (row.widespread != null ? row.widespread  + ' - ' : "")
+          + (row.peribucal != null ? row.peribucal  + ' - ' : "")
+          + (row.periorbitary != null ? row.periorbitary  + ' - ' : "")
+          + (row.none != null ? row.none  + ' - ' : "")
+          + (row.intercostal != null ? row.intercostal  + ' - ' : "")
+          + (row.aupraclavicular != null ? row.aupraclavicular: "")
+          ;
+        },
+    },
+  }
+  };
+
+
+  constructor(
+    public userChangeS: UserChangeService,
+    public datePipe: DateFormatPipe,
+  ) {
+  }
+
+  async ngOnInit() {
+    console.log('signos');
+  }
+
+  RefreshData() {
+    this.table.refresh();
+  }
+
+  receiveMessage($event) {
+    if ($event == true) {
+      this.RefreshData();
+      if (this.type_record_id == 1) {
+        this.messageEvent.emit(true);
+      }
+    }
+  }
+}
---
+++ src/app/pages/clinic-history/respiratory-therapy-list/entry-respiratory-therapy/assessment-therapy/diagnostic-aids/diagnostic-aids.component.ts
@@ -0,0 +1,80 @@
+import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { BaseTableComponent } from '../../../../../components/base-table/base-table.component';
+import { UserChangeService } from '../../../../../../business-controller/user-change.service';
+import { DateFormatPipe } from '../../../../../../pipe/date-format.pipe';
+
+@Component({
+  selector: 'ngx-diagnostic-aids',
+  templateUrl: './diagnostic-aids.component.html',
+  styleUrls: ['./diagnostic-aids.component.scss'],
+})
+export class DiagnosticAidsComponent implements OnInit {
+  @ViewChild(BaseTableComponent) table: BaseTableComponent;
+  @Input() record_id;
+  @Input() type_record: any = null;
+  @Input() type_record_id: any = null;
+  @Input() has_input: boolean = false;
+  @Output() messageEvent = new EventEmitter<any>();
+
+  linearMode = true;
+  public messageError = null;
+  public title: string = '';
+  public subtitle: string = '';
+  public headerFields: any[] = ['Fecha', 'Ayuda diagnóstica', 'Observación'];
+  public routes = [];
+  public data = [];
+  public loading: boolean = false;
+  public saved: any = null;
+  public isSubmitted = false;
+  public all_changes: any[];
+  public saveEntry: any = 0;
+
+
+  public settings = {
+    pager: {
+      display: true,
+      perPage: 30,
+    },
+
+    columns: {
+      created_at: {
+        title: this.headerFields[0],
+        type: 'string',
+        valuePrepareFunction: (value, row) => {
+          return this.datePipe.transform4(value);
+        },
+      },
+      paraclinical: {
+        title: this.headerFields[1],
+        width: 'string',
+      },
+      observation: {
+        title: this.headerFields[2],
+        width: 'string',
+      },
+    }
+  };
+
+
+  constructor(
+    public userChangeS: UserChangeService,
+    public datePipe: DateFormatPipe,
+  ) {
+  }
+
+  async ngOnInit() {
+  }
+
+  RefreshData() {
+    this.table.refresh();
+  }
+
+  receiveMessage($event) {
+    if ($event == true) {
+      this.RefreshData();
+      if (this.type_record_id == 1) {
+        this.messageEvent.emit(true);
+      }
+    }
+  }
+}
